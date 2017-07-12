cf6028ef5147a966fc29759aa36ce199ede71fb5
Gary Wu <gary.i.wu@huawei.com>
2017-07-12 14:53:01 -0700

    Sample CSIT test suite
    Change-Id: I07f685cb13cea2996e97fab4c65441bbd39f9539
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/csit/scripts/build-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# $1 docker image path under ${WORKSPACE}/test/csit/docker/
+
+# only rebuild if not already existing
+if [ "$(docker images -q $1 2> /dev/null)" == "" ]; then
+    docker build -t $1 $WORKSPACE/test/csit/docker/$1
+fi
