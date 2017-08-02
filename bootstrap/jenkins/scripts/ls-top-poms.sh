5cccfa9ffc8c1bb487763571149b71062e55110c
Gary Wu <gary.i.wu@huawei.com>
2017-08-02 12:03:10 -0700

    Add license header to vagrant-jenkins scripts
    Change-Id: I506c964f1ea85e3336d9b359751940b568c34506
    Issue-id: INT-53
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- bootstrap/jenkins/scripts/ls-top-poms.sh
+++ bootstrap/jenkins/scripts/ls-top-poms.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+#
+# Copyright 2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
 find $1 -mindepth 0 -type d -exec test -e "{}/pom.xml" ';' -prune -printf "%P\n" | sort
