83f39ae97c88f79a73617606336ff2a120a7667b
Nate Potter <nathaniel.potter@intel.com>
2017-08-02 15:17:41 -0700

    Add warnings and options to unit_testing.sh
    Adds a warning to unit_testing.sh informing the user that
    the contents of their /opt folder will be deleted by the test.
    Also adds option parsing, -y for skipping the prompt and -d
    for adding xtrace debugging.
    
    Change-Id: I888350d31839bd6db91a497a04ae3308af5329e9
    Signed-off-by: Nate Potter <nathaniel.potter@intel.com>

--- bootstrap/vagrant-onap/unit_testing.sh
+++ bootstrap/vagrant-onap/unit_testing.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
-set -o xtrace
+run=false
+
+while getopts "yd" OPTION; do
+    case $OPTION in
+        y)
+            run=true
+            ;;
+        d)
+            set -o xtrace
+            ;;
+    esac
+done
+
+if [ "$run" = false ] ; then
+    while true; do
+        echo "Warning: This test script will delete the contents of your /opt folder."
+        read -p "Would you like to continue? [y]es/[n]o" yn
+        case $yn in
+            [Yy]*)
+                break
+                ;;
+            [Nn]*)
+                exit
+                ;;
+        esac
+    done
+fi
 
 rm -rf /opt/
 rm -rf /root/.m2/
