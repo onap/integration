1346b687b8615f60ca8419c55512af5b3152a2f1
Gary Wu <gary.i.wu@huawei.com>
2017-08-08 08:04:44 -0700

    Add docker login to nexus3 in CSIT run script
    Change-Id: I8208a3c45791d46699a4545f12d36a0dbaba82cf
    Issue-id: INT-62
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/csit/run-csit.sh
+++ test/csit/run-csit.sh
@@ -107,6 +107,9 @@ export PATH=${PATH}:${WORKSPACE}/test/csit/docker/scripts:${WORKSPACE}/test/csit
 export SCRIPTS=${WORKSPACE}/test/csit/scripts
 export ROBOT_VARIABLES=
 
+# Sign in to nexus3 docker repo
+docker login -u anonymous -p anonymous nexus3.onap.org:10001
+
 # Run setup script plan if it exists
 cd ${TESTPLANDIR}
 SETUP=${TESTPLANDIR}/setup.sh
