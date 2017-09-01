6e99bdc32da920b23ee06e90cb6fe4d2faa04642
Gary Wu <gary.i.wu@huawei.com>
2017-09-01 12:51:50 -0700

    Refactor manifest directory structure
    Change-Id: I8398d7d436e042ff9b4dcb3fd0dfb392b3d733b0
    Issue-ID: INT-124
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- version-manifest/src/main/java/org/onap/integration/versioncheck/VersionCheckMojo.java
+++ version-manifest/src/main/java/org/onap/integration/versioncheck/VersionCheckMojo.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.onap.oparent.versioncheck;
+package org.onap.integration.versioncheck;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
