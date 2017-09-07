4fd0bec77df9b1561efdba3aa184b4cb93a11cba
HuabingZhao <zhao.huabing@zte.com.cn>
2017-09-07 16:19:14 +0800

    Fix csit issue
    Remove sudo when start docker container.
    
    Issue-Id: MSB-61
    Change-Id: Id34d55e0e9d0f766a96c710aa730fef210ede510
    Signed-off-by: HuabingZhao <zhao.huabing@zte.com.cn>

--- test/csit/plans/msb/sanity-check/setup.sh
+++ test/csit/plans/msb/sanity-check/setup.sh
@@ -18,15 +18,15 @@
 source ${SCRIPTS}/common_functions.sh
 
 #start msb
-sudo docker run -d -p 8500:8500  --name msb_consul consul
+docker run -d -p 8500:8500  --name msb_consul consul
 MSB_CONSUL_IP=`get-instance-ip.sh msb_consul`
 echo MSB_CONSUL_IP=${MSB_CONSUL_IP}
 
-sudo docker run -d  -p 10081:10081  -e CONSUL_IP=$MSB_CONSUL_IP --name msb_discovery nexus3.onap.org:10001/onap/msb/msb_discovery
+docker run -d  -p 10081:10081  -e CONSUL_IP=$MSB_CONSUL_IP --name msb_discovery nexus3.onap.org:10001/onap/msb/msb_discovery
 MSB_DISCOVERY_IP=`get-instance-ip.sh msb_discovery`
 echo DISCOVERY_IP=${MSB_DISCOVERY_IP}
 
-sudo docker run -d -p 80:80 -e CONSUL_IP=$MSB_CONSUL_IP -e SDCLIENT_IP=$MSB_DISCOVERY_IP --name msb_internal_apigateway nexus3.onap.org:10001/onap/msb/msb_apigateway
+docker run -d -p 80:80 -e CONSUL_IP=$MSB_CONSUL_IP -e SDCLIENT_IP=$MSB_DISCOVERY_IP --name msb_internal_apigateway nexus3.onap.org:10001/onap/msb/msb_apigateway
 MSB_IAG_IP=`get-instance-ip.sh msb_internal_apigateway`
 echo MSB_IAG_IP=${MSB_IAG_IP}
 
