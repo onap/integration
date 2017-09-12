168d9efc53fcb56195d2dd7cdc50e94b087ef8bb
luxin <luxin7@huawei.com>
2017-09-12 17:53:20 +0800

    Modify svnfm CSIT
    Change-Id: I666f4c94ebcecd3e78108d9db7ec8ba2548a56d1
    Issue-Id:VFC-327
    Signed-off-by: luxin <luxin7@huawei.com>

--- test/csit/plans/vfc-nfvo-driver-vnfm-svnfm/sanity-check/setup.sh
+++ test/csit/plans/vfc-nfvo-driver-vnfm-svnfm/sanity-check/setup.sh
@@ -70,5 +70,14 @@ echo "================================================"
 docker cp vfc-ztevmanagerdriver:/service/vfc/nfvo/driver/vnfm/svnfm/zte/vmanager/docker/instance_run.sh ./
 cat instance_run.sh
 
+# Start svnfm-huawei
+docker run -d --name vfc-svnfm-huawei -e MSB_ADDR=${MSB_IAG_IP}:80 nexus3.onap.org:10001/onap/vfc/nfvo/svnfm/huawei
+SERVICE_IP=`get-instance-ip.sh vfc-svnfm-huawei`
+for i in {1..10}; do
+    curl -sS ${SERVICE_IP}:8482 && break
+    echo sleep $i
+    sleep $i
+done
+
 # Pass any variables required by Robot test suites in ROBOT_VARIABLES
-ROBOT_VARIABLES="-v MSB_IAG_IP:${MSB_IAG_IP} -v MSB_DISCOVERY_IP:${MSB_DISCOVERY_IP} -v ZTEVMANAGERDRIVER_IP:${ZTEVMANAGERDRIVER_IP}"
+ROBOT_VARIABLES="-v MSB_IAG_IP:${MSB_IAG_IP} -v MSB_DISCOVERY_IP:${MSB_DISCOVERY_IP} -v ZTEVMANAGERDRIVER_IP:${ZTEVMANAGERDRIVER_IP} -v MSB_IP:${MSB_IAG_IP} -v SERVICE_IP:${SERVICE_IP}"
--- test/csit/plans/vfc-nfvo-driver-vnfm-svnfm/sanity-check/teardown.sh
+++ test/csit/plans/vfc-nfvo-driver-vnfm-svnfm/sanity-check/teardown.sh
@@ -21,4 +21,4 @@ kill-instance.sh msb_discovery
 kill-instance.sh msb_consul
 kill-instance.sh  vfc-ztevmanagerdriver
 
-# kill-instance.sh vfc-svnfm-huawei
+kill-instance.sh vfc-svnfm-huawei
