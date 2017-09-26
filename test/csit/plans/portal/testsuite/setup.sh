cd25fc2f63e9c62f5a983bc6dd04c45a74776c2a
kishore <kk707x@att.com>
2017-09-26 18:06:16 -0500

    Add updated setup.sh env
    Issue-ID:PORTAL-53
    Signed-off-by: kishore <kk707x@att.com>
    Change-Id: I4af5150fcbc455f0ad180f614584a4bcd75ea391

--- test/csit/plans/portal/testsuite/setup.sh
+++ test/csit/plans/portal/testsuite/setup.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
-# Starts docker containers for ONAP Portal in Rackspace.
-# Version for Amsterdam/R1 uses docker-compose.
+# Starts docker containers for ONAP Portal
+# This version for Amsterdam/R1 of Portal, uses docker-compose.
+# Temporarily maintained in portal/deliveries area;
+# replicated from the ONAP demo/boot area due to release concerns.
+
+# Start Xvfb
+echo -e "Starting Xvfb on display ${DISPLAY} with res ${RES}"
+Xvfb ${DISPLAY} -ac -screen 0 ${RES} +extension RANDR &
+XVFBPID=$!
+# Get pid of this spawned process to make sure we kill the correct process later
+
+#Get current IP of VM
+HOST_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
+export HOST_IP=${HOST_IP}
+
 
 # be verbose
 set -x
 
 # Establish environment variables
-NEXUS_USERNAME=$(cat /opt/config/nexus_username.txt)
-NEXUS_PASSWD=$(cat /opt/config/nexus_password.txt)
-NEXUS_DOCKER_REPO=$(cat /opt/config/nexus_docker_repo.txt)
-DOCKER_IMAGE_VERSION=$(cat /opt/config/docker_version.txt)
+NEXUS_USERNAME=docker
+NEXUS_PASSWD=docker
+NEXUS_DOCKER_REPO=nexus3.onap.org:10003
+
+
+
+CURR="$(pwd)"
+git clone http://gerrit.onap.org/r/portal
 
 # Refresh configuration and scripts
-cd /opt/portal
+cd portal
 git pull
 cd deliveries
 
 # Get image names used below from docker-compose environment file
-source .env
+source $CURR/.env
 
 # Copy property files
 ETC=/PROJECT/OpenSource/UbuntuEP/etc
@@ -35,6 +52,15 @@ docker tag $NEXUS_DOCKER_REPO/openecomp/${DB_IMG_NAME}:$DOCKER_IMAGE_VERSION $DB
 docker tag $NEXUS_DOCKER_REPO/openecomp/${EP_IMG_NAME}:$DOCKER_IMAGE_VERSION $EP_IMG_NAME:$PORTAL_TAG
 docker tag $NEXUS_DOCKER_REPO/openecomp/${WMS_IMG_NAME}:$DOCKER_IMAGE_VERSION $WMS_IMG_NAME:$PORTAL_TAG
 
-# docker-compose is not in /usr/bin
-/opt/docker/docker-compose down
-/opt/docker/docker-compose up -d
+# compose is not in /usr/bin
+docker-compose down
+docker-compose up -d
+
+#Get current IP of VM
+HOST_IP=$(ip route get 8.8.8.8 | awk '/8.8.8.8/ {print $NF}')
+export HOST_IP=${HOST_IP}
+
+
+
+
+
