a867833d0943b36f431b869c630959aa23abaf0f
Gary Wu <gary.i.wu@huawei.com>
2017-10-19 12:39:19 -0700

    Script to get floating IP for a server name
    Change-Id: I6608c075c6c1adfa1a088f5da70619ebd773ffa5
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/ete/scripts/get-floating-ip.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Get floating IP assigned to a server name
+
+PORT_ID=$(openstack server show -f json $1 | python -c 'import sys, json; print json.load(sys.stdin)["wrs-if:nics"][0]["nic1"]["port_id"]')
+FLOATING_IP=$(openstack floating ip list -f json --port $PORT_ID | python -c 'import sys, json; print json.load(sys.stdin)[0]["Floating IP Address"]')
+echo $FLOATING_IP
