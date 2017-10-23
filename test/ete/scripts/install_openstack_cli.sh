838c34d02bcc34576dc660058a2beb08add8f334
Gary Wu <gary.i.wu@huawei.com>
2017-10-23 14:22:40 -0700

    Refactor deployment scripts
    Change-Id: Ib82ddeb13902afdc50de61e1042cf8b810581509
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ test/ete/scripts/install_openstack_cli.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ -z "$OS_AUTH_URL" ] || [ -z "$OS_USERNAME" ]
+then
+    echo "ERROR: OpenStack environment variables not set.  Please source your OpenStack RC script first."
+    exit 1
+fi
+
+
+if [ -z "$WORKSPACE" ]; then
+    export WORKSPACE=`git rev-parse --show-toplevel`
+fi
+
+
+
+# Assume that if ROBOT_VENV is set, we don't need to reinstall robot
+if [ -f ${WORKSPACE}/env.properties ]; then
+    source ${WORKSPACE}/env.properties
+fi
+
+if [ ! -z "$ONAP_VENV" ] && [ -f "$ONAP_VENV/bin/activate" ]; then
+    source ${ONAP_VENV}/bin/activate
+else
+    ONAP_VENV=$(mktemp -d --suffix=_onap_venv)
+    virtualenv ${ONAP_VENV}
+    source ${ONAP_VENV}/bin/activate
+    
+    pip install --upgrade pip
+    pip install --upgrade python-openstackclient python-heatclient
+    
+    echo "ONAP_VENV=${ONAP_VENV}" >> $WORKSPACE/env.properties
+fi
+echo "ONAP_VENV=${ONAP_VENV}"
+
+if [ -z "$ONAP_WORKDIR" ]; then
+    ONAP_WORKDIR=$(mktemp -d --suffix=_onap_workdir)
+    echo "ONAP_WORKDIR=${ONAP_WORKDIR}" >> $WORKSPACE/env.properties
+fi
+echo "ONAP_WORKDIR=${ONAP_WORKDIR}"
+if [ ! -d ${ONAP_WORKDIR}/demo ]; then
+    git clone http://gerrit.onap.org/r/demo ${ONAP_WORKDIR}/demo
+else
+    pushd ${ONAP_WORKDIR}/demo
+    git pull
+    popd
+fi
+
