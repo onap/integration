6642b8f93e0a109c2f85da27dc54453a833e070c
Gary Wu <gary.i.wu@huawei.com>
2017-10-27 11:21:56 -0700

    Allow root login on robot VM for ETE
    Change-Id: I0c2c44c043d5aa05eb347f6449fbc4c646d56bb1
    Issue-ID: INT-310
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/run-healthcheck.sh
+++ test/ete/scripts/run-healthcheck.sh
@@ -13,6 +13,9 @@ cd $WORKSPACE/test/ete/scripts
 ROBOT_IP=$(./get-floating-ip.sh onap-robot)
 echo "ROBOT_IP=${ROBOT_IP}"
 
+# allow direct login as root
+ssh -o StrictHostKeychecking=no -i ${SSH_KEY} ubuntu@${ROBOT_IP} 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/'
+
 ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PASSWORD_INPUT=$OS_PASSWORD_INPUT bash -s" < ./remote/run-robot.sh
 LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | head -1")
 rsync -e "ssh -i ${SSH_KEY}" -avPz root@${ROBOT_IP}:/opt/eteshare/logs/${LOG_DIR}/ $WORKSPACE/archives/
