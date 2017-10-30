35ed5406fd12a20dc48a1ec010f22c96f05cbf24
Gary Wu <gary.i.wu@huawei.com>
2017-10-30 14:06:09 -0700

    Show Robot website URL for log directory
    Change-Id: Id5ef356ae8678a121d83a5efbf991619f99cddde
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/run-healthcheck.sh
+++ test/ete/scripts/run-healthcheck.sh
@@ -18,4 +18,5 @@ ssh -o StrictHostKeychecking=no -i ${SSH_KEY} ubuntu@${ROBOT_IP} 'sudo cp /home/
 
 ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PASSWORD_INPUT=$OS_PASSWORD_INPUT bash -s" < ./remote/run-robot.sh
 LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | head -1")
+echo "Browse Robot results at http://${ROBOT_IP}:88/logs/${LOG_DIR}/"
 rsync -e "ssh -i ${SSH_KEY}" -avPz root@${ROBOT_IP}:/opt/eteshare/logs/${LOG_DIR}/ $WORKSPACE/archives/
