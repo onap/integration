bd894605b23edf0ed796540bbc6fa80f33030005
Gary Wu <gary.i.wu@huawei.com>
2017-10-30 11:52:40 -0700

    Update .env file to match latest heat template
    Change-Id: Ia16b1f2cc8c399f47bb20ae9f1df7e492fe9baf5
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/deploy-onap.sh
+++ test/ete/scripts/deploy-onap.sh
@@ -8,13 +8,18 @@ source $WORKSPACE/test/ete/scripts/install_openstack_cli.sh
 
 # Delete all existing stacks
 STACKS=$(openstack stack list -c "Stack Name" -f value)
-echo "Deleting Stacks ${STACKS}"
-openstack stack delete -y $STACKS
+if [ ! -z "${STACKS}" ]; then
+    echo "Deleting Stacks ${STACKS}"
+    openstack stack delete -y $STACKS
+else
+    echo "No existing stacks to delete."
+fi
 
 STACK="ete-$(uuidgen | cut -c-8)"
 echo "New Stack Name: ${STACK}"
 
 
+cp ${ONAP_WORKDIR}/demo/heat/ONAP/onap_openstack.env ${WORKSPACE}/test/ete/labs/windriver/onap-openstack-demo.env
 envsubst < ${WORKSPACE}/test/ete/labs/windriver/onap-openstack-template.env > ${WORKSPACE}/test/ete/labs/windriver/onap-openstack.env
 
 openstack stack create -t ${ONAP_WORKDIR}/demo/heat/ONAP/onap_openstack.yaml -e ${WORKSPACE}/test/ete/labs/windriver/onap-openstack.env $STACK
