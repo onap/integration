b4cef0b8b591495a5378ac63c34cef665401f088
Gary Wu <gary.i.wu@huawei.com>
2017-11-09 10:59:46 -0800

    Add wait loops for stack delete/create completion
    Change-Id: If61d54b01e5f90dc75f3f8ec29914b7b506f3c51
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/deploy-onap.sh
+++ test/ete/scripts/deploy-onap.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 if [ -z "$WORKSPACE" ]; then
     export WORKSPACE=`git rev-parse --show-toplevel`
@@ -8,12 +8,21 @@ source $WORKSPACE/test/ete/scripts/install_openstack_cli.sh
 
 # Delete all existing stacks
 STACKS=$(openstack stack list -c "Stack Name" -f value)
+
 if [ ! -z "${STACKS}" ]; then
     echo "Deleting Stacks ${STACKS}"
     openstack stack delete -y $STACKS
+    for STACK in ${STACKS}; do
+        until ! openstack stack show -c stack_status -f value $STACK; do
+            sleep 30
+        done
+    done
 else
     echo "No existing stacks to delete."
 fi
+
+
+
 
 STACK="ete-$(uuidgen | cut -c-8)"
 echo "New Stack Name: ${STACK}"
@@ -21,6 +30,14 @@ echo "New Stack Name: ${STACK}"
 
 cp ${ONAP_WORKDIR}/demo/heat/ONAP/onap_openstack.env ${WORKSPACE}/test/ete/labs/windriver/onap-openstack-demo.env
 envsubst < ${WORKSPACE}/test/ete/labs/windriver/onap-openstack-template.env > ${WORKSPACE}/test/ete/labs/windriver/onap-openstack.env
+diff ${WORKSPACE}/test/ete/labs/windriver/onap-openstack-template.env ${WORKSPACE}/test/ete/labs/windriver/onap-openstack.env
 
 openstack stack create -t ${ONAP_WORKDIR}/demo/heat/ONAP/onap_openstack.yaml -e ${WORKSPACE}/test/ete/labs/windriver/onap-openstack.env $STACK
 
+while [ "CREATE_IN_PROGRESS" == "$(openstack stack show -c stack_status -f value $STACK)" ]; do
+    sleep 10
+done
+
+STATUS=$(openstack stack show -c stack_status -f value $STACK)
+echo $STATUS
+[ "CREATE_COMPLETE" == "$STATUS" ]
