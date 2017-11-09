e8ac9b5c3bcef7a4f9737beaecca703fff95394a
Gary Wu <gary.i.wu@huawei.com>
2017-11-09 10:08:27 -0800

    Skip checking of ROBOT VM host keys
    Change-Id: Iaefe5974d1544faf7117126d561e3c7bb6a0a42e
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/run-healthcheck.sh
+++ test/ete/scripts/run-healthcheck.sh
@@ -13,8 +13,7 @@ cd $WORKSPACE/test/ete/scripts
 ROBOT_IP=$(./get-floating-ip.sh onap-robot)
 echo "ROBOT_IP=${ROBOT_IP}"
 
-# allow direct login as root
-ssh -o StrictHostKeychecking=no -i ${SSH_KEY} ubuntu@${ROBOT_IP} 'sudo cp /home/ubuntu/.ssh/authorized_keys /root/.ssh/'
+ssh-keygen -R ${ROBOT_IP}
 
 ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PASSWORD_INPUT=$OS_PASSWORD_INPUT bash -s" < ./remote/run-robot.sh
 LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | head -1")
