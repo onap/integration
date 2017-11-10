e1a036492494c1097dbebb42fa4378bd4b10f3ec
Gary Wu <gary.i.wu@huawei.com>
2017-11-09 16:42:03 -0800

    Pass username/password to remote script
    Change-Id: I3b044463d2492bdb6abe60df9d06da7716bc9452
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/run-healthcheck.sh
+++ test/ete/scripts/run-healthcheck.sh
@@ -19,7 +19,7 @@ fi
 
 ssh-keygen -R ${ROBOT_IP}
 
-ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PASSWORD_INPUT=$OS_PASSWORD_INPUT bash -s" < ./remote/run-robot.sh
+ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PROJECT_ID=$OS_PROJECT_ID OS_USERNAME=$OS_USERNAME OS_PASSWORD=$OS_PASSWORD bash -s" < ./remote/run-robot.sh
 
 LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | head -1")
 echo "Browse Robot results at http://${ROBOT_IP}:88/logs/${LOG_DIR}/"
