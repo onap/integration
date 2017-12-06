0793d6c94e9c8e71af02df2bd40524d3e7136e53
Gary Wu <gary.i.wu@huawei.com>
2017-12-06 10:19:52 -0800

    Use amsterdam branch heat template
    Change-Id: I62d502fb6c172aef88d7e379b654c559b7c641a4
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/install_openstack_cli.sh
+++ test/ete/scripts/install_openstack_cli.sh
@@ -39,8 +39,12 @@ fi
 echo "ONAP_WORKDIR=${ONAP_WORKDIR}"
 if [ ! -d ${ONAP_WORKDIR}/demo ]; then
     git clone https://gerrit.onap.org/r/demo ${ONAP_WORKDIR}/demo
+    pushd ${ONAP_WORKDIR}/demo
+    git checkout amsterdam
+    popd
 else
     pushd ${ONAP_WORKDIR}/demo
+    git checkout amsterdam
     git pull
     popd
 fi
