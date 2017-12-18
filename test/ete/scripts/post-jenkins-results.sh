d777054cd0cb8b436e831c86e406753d7a155e12
Gary Wu <gary.i.wu@huawei.com>
2017-12-18 10:09:15 -0800

    Update Jenkins API Token
    Change-Id: I3e5cabae39ff4ce9b96bf2e978257398cd8d759c
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/post-jenkins-results.sh
+++ test/ete/scripts/post-jenkins-results.sh
@@ -2,5 +2,5 @@
 
 CRUMB=$(curl -s -u "lf:lf" 'http://12.234.32.117/jenkins/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)')
 
-curl -v -u "lf:929b8c2bea4c4c6847778f8cd6241cbf" -H "$CRUMB" -d '<run><log encoding="hexBinary">4142430A</log><result>0</result><duration>17</duration></run>' http://12.234.32.117/jenkins/job/external-job/postBuildResult
+curl -v -u "lf:eea50a6d845752e1d2fa459a3c0ca25e" -H "$CRUMB" -d '<run><log encoding="hexBinary">4142430A</log><result>0</result><duration>17</duration></run>' http://12.234.32.117/jenkins/job/external-job/postBuildResult
 
