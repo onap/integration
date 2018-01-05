ab1fd5361b603ed4a6db7b9c6495a819b052d21a
Gary Wu <gary.i.wu@huawei.com>
2018-01-05 14:04:51 -0800

    Refactor script to retrieve instance floating IP
    Change-Id: Iacfe44feb72456a78372422937645f8ecc92888b
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/get-floating-ip.sh
+++ test/ete/scripts/get-floating-ip.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 # Get floating IP assigned to a server name
-
-PORT_ID=$(openstack server show -f json $1 | python -c 'import sys, json; print json.load(sys.stdin)["wrs-if:nics"][0]["nic1"]["port_id"]')
-FLOATING_IP=$(openstack floating ip list -f json --port $PORT_ID | python -c 'import sys, json; print json.load(sys.stdin)[0]["Floating IP Address"]')
-echo $FLOATING_IP
+openstack server show -c addresses -f json $1 | jq -r '.addresses' | tr -d ' ' | cut -d ',' -f 2
