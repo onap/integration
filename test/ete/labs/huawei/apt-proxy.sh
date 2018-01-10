5e8e8128b2cbb961e138704f791326e8d0d974e1
Gary Wu <gary.i.wu@huawei.com>
2018-01-09 19:54:45 -0800

    Use lab local docker proxies
    Change-Id: Ifb7821d6173d16ccb59053edd9eb53632fe65933
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit f58cbcbc8603db69e1bac6b34d1d5aaf88470471)

--- test/ete/labs/huawei/apt-proxy.sh
+++ test/ete/labs/huawei/apt-proxy.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 sed -i '/#!\/bin\/bash/a\
+            mkdir -p /etc/docker\
+            cat > /etc/docker/daemon.json <<EOF\
+            {\
+              "insecure-registries" : ["docker-proxy.neo.futurewei.com:5000"]\
+            }\
+            EOF\
             cat > /etc/apt/apt.conf.d/30proxy<<EOF\
-            Acquire::http { Proxy "http://10.145.122.118:3142"; };\
+            Acquire::http { Proxy "http://apt-proxy.neo.futurewei.com:3142"; };\
             Acquire::https::Proxy "DIRECT";\
             EOF\
             apt-get -y update' $1
