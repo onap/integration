453ca898ef1d80af91767d7481bc3b1dbee03d41
Gary Wu <gary.i.wu@huawei.com>
2018-01-09 23:08:14 -0800

    Use apt and docker proxy for windriver lab
    Change-Id: Ic800877cd3c939606655f834e491b72d93eebf5e
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>
    (cherry picked from commit 341e0b5d4bb62380e77095cc22764c860706f645)

---
+++ test/ete/labs/windriver/apt-proxy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sed -i '/#!\/bin\/bash/a\
+            mkdir -p /etc/docker\
+            cat > /etc/docker/daemon.json <<EOF\
+            {\
+              "insecure-registries" : ["10.12.5.80:5000"]\
+            }\
+            EOF\
+            cat > /etc/apt/apt.conf.d/30proxy<<EOF\
+            Acquire::http { Proxy "http://10.12.5.80:3142"; };\
+            Acquire::https::Proxy "DIRECT";\
+            EOF\
+            apt-get -y update' $1
