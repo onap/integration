f58cbcbc8603db69e1bac6b34d1d5aaf88470471
Gary Wu <gary.i.wu@huawei.com>
2018-01-09 19:54:45 -0800

    Use lab local docker proxies
    Change-Id: Ifb7821d6173d16ccb59053edd9eb53632fe65933
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/labs/gwu/apt-proxy.sh
+++ test/ete/labs/gwu/apt-proxy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 sed -i '/#!\/bin\/bash/a\
+            mkdir -p /etc/docker\
+            cat > /etc/docker/daemon.json <<EOF\
+            {\
+              "insecure-registries" : ["stack.local.enacct.com:5000"]\
+            }\
+            EOF\
             cat > /etc/apt/apt.conf.d/30proxy<<EOF\
             Acquire::http { Proxy "http://stack.local.enacct.com:3142"; };\
             Acquire::https::Proxy "DIRECT";\
