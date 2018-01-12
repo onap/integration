70bcc2f9c7d60f9f4b838665aa93898639f83a28
Gary Wu <gary.i.wu@huawei.com>
2018-01-12 15:12:06 -0800

    Update lab environment configuration
    Change-Id: I896b74b9e5e734b4beb35130f32709c9b5ee3ac8
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/labs/gwu/apt-proxy.sh
+++ test/ete/labs/gwu/apt-proxy.sh
@@ -5,11 +5,11 @@ sed -i '/#!\/bin\/bash/a\
             mkdir -p /etc/docker\
             cat > /etc/docker/daemon.json <<EOF\
             {\
-              "insecure-registries" : ["docker-proxy.local.enacct.com:5000"]\
+              "insecure-registries" : ["force.local.enacct.com:5000"]\
             }\
             EOF\
             cat > /etc/apt/apt.conf.d/30proxy<<EOF\
-            Acquire::http { Proxy "http://stack.local.enacct.com:3142"; };\
+            Acquire::http { Proxy "http://force.local.enacct.com:3142"; };\
             Acquire::https::Proxy "DIRECT";\
             EOF\
             apt-get -y update' $1
