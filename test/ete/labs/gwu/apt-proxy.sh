6177809fcbe19e5fa9d360e4ee381534e947ecd9
Gary Wu <gary.i.wu@huawei.com>
2018-01-15 14:23:37 -0800

    Use numeric IP addresses for lab end points
    Change-Id: Iec97b837ef83a903605a1ce07db68e3f3c65310c
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/labs/gwu/apt-proxy.sh
+++ test/ete/labs/gwu/apt-proxy.sh
@@ -5,11 +5,11 @@ sed -i '/#!\/bin\/bash/a\
             mkdir -p /etc/docker\
             cat > /etc/docker/daemon.json <<EOF\
             {\
-              "insecure-registries" : ["force.local.enacct.com:5000"]\
+              "insecure-registries" : ["192.168.1.51:5000"]\
             }\
             EOF\
             cat > /etc/apt/apt.conf.d/30proxy<<EOF\
-            Acquire::http { Proxy "http://force.local.enacct.com:3142"; };\
+            Acquire::http { Proxy "http://192.168.1.51:3142"; };\
             Acquire::https::Proxy "DIRECT";\
             EOF\
             apt-get -y update' $1
