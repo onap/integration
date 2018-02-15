227c60235a47ef667b9641fa16e3a832ba2a183b
Gary Wu <gary.i.wu@huawei.com>
2018-02-14 17:12:35 -0800

    Change IP addr of docker proxy in windriver lab
    Change-Id: I23640c855aa30a4191bc2bf023d1c19cf505ecca
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/labs/windriver/apt-proxy.sh
+++ test/ete/labs/windriver/apt-proxy.sh
@@ -3,11 +3,11 @@ sed -i '/#!\/bin\/bash/a\
             mkdir -p /etc/docker\
             cat > /etc/docker/daemon.json <<EOF\
             {\
-              "insecure-registries" : ["10.12.5.80:5000"]\
+              "insecure-registries" : ["10.12.5.2:5000"]\
             }\
             EOF\
             cat > /etc/apt/apt.conf.d/30proxy<<EOF\
-            Acquire::http { Proxy "http://10.12.5.80:3142"; };\
+            Acquire::http { Proxy "http://10.12.5.2:3142"; };\
             Acquire::https::Proxy "DIRECT";\
             EOF\
             apt-get -y update' $1
