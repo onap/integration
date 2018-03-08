3a275ec6f09bb6e926b933f5138ba5e2fffbf77b
Leonardo Bellini <leonardo.bellini@att.com>
2018-03-08 11:48:47 -0600

    freeze optf/has for m3
    Issue-ID: OPTFRA-165
    
    Change-Id: I9b041808eb40bab42436811cdc34c9f99257ad9c
    Signed-off-by: Leonardo Bellini <leonardo.bellini@att.com>

--- test/csit/scripts/optf/has/has_script.sh
+++ test/csit/scripts/optf/has/has_script.sh
@@ -14,12 +14,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-echo "######################################################################"
-echo "### "
 echo "### This is ${WORKSPACE}/test/csit/scripts/optf/has/has_script.sh"
-echo "### "
-echo "######################################################################"
 #
 # add here whatever commands is needed to prepare the optf/has CSIT testing
 #
 
+# assume the base is /opt
+DIR=/opt
+#cd /opt
+
+# the directory of the script
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo ${DIR}
+cd ${DIR}
+
+# the temp directory used, within $DIR
+# omit the -p parameter to create a temporal directory in the default location
+WORK_DIR=`mktemp -d -p "$DIR"`
+echo ${WORK_DIR}
+
+cd ${WORK_DIR}
+
+# check if tmp dir was created
+if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
+  echo "Could not create temp dir"
+  exit 1
+fi
+
+
+git clone https://gerrit.onap.org/r/optf/has
+cd has
+cd conductor/docker
+
+sed  -i -e "s%FROM python:2\.7%FROM python:2\.7\\nENV http_proxy http:\/\/one\.proxy\.att\.com:8080\\nENV https_proxy http:\/\/one\.proxy\.att\.com:8080%g" api/Dockerfile
+sed  -i -e "s%FROM python:2\.7%FROM python:2\.7\\nENV http_proxy http:\/\/one\.proxy\.att\.com:8080\\nENV https_proxy http:\/\/one\.proxy\.att\.com:8080%g" controller/Dockerfile
+sed  -i -e "s%FROM python:2\.7%FROM python:2\.7\\nENV http_proxy http:\/\/one\.proxy\.att\.com:8080\\nENV https_proxy http:\/\/one\.proxy\.att\.com:8080%g" data/Dockerfile
+sed  -i -e "s%FROM python:2\.7%FROM python:2\.7\\nENV http_proxy http:\/\/one\.proxy\.att\.com:8080\\nENV https_proxy http:\/\/one\.proxy\.att\.com:8080%g" reservation/Dockerfile
+sed  -i -e "s%FROM python:2\.7%FROM python:2\.7\\nENV http_proxy http:\/\/one\.proxy\.att\.com:8080\\nENV https_proxy http:\/\/one\.proxy\.att\.com:8080%g" solver/Dockerfile
+
+
+# ./build-dockers.sh
+docker build -t api api/
+docker build -t controller controller/
+docker build -t data data/
+docker build -t solver solver/
+docker build -t reservation reservation/
+
+
+# create directory for volume and copy configuration file
+mkdir -p /tmp/conductor/properties
+cp ${WORKSPACE}/test/csit/scripts/optf/has/has-properties/conductor.conf.onap /tmp/conductor/properties/conductor.conf
+cp ${WORKSPACE}/test/csit/scripts/optf/has/has-properties/cert.cer /tmp/conductor/properties/cert.cer
+cp ${WORKSPACE}/test/csit/scripts/optf/has/has-properties/cert.key /tmp/conductor/properties/cert.key
+
+
+
+# run docker containers
+#docker run -d --name cond-data -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf -v /tmp/conductor/properties/cert.key:/usr/local/bin/cert.key -v /tmp/conductor/properties/cert.cer:/usr/local/bin/cert.cer data
+#docker run -d --name cond-data -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf data
+docker run -d --name cond-cont -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf controller
+docker run -d --name cond-api -p 8091:8091  -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf api
+docker run -d --name cond-solv -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf solver
+docker run -d --name cond-resv -v /tmp/conductor/properties/conductor.conf:/usr/local/bin/conductor.conf reservation
+
+# wait a while before continuing
+sleep 30
--- test/csit/tests/optf/has/optf_has_test.robot
+++ test/csit/tests/optf/has/optf_has_test.robot
@@ -13,11 +13,47 @@ ${RESP_MESSAGE_WITHOUT_DEMANDS}    Undefined Demand
 ${generatedPlanId}
 
 *** Test Cases ***
+Check ConductorApi Docker Container
+    [Documentation]    It checks cassandra docker container is running
+    ${rc}    ${output}=    Run and Return RC and Output    docker ps
+    Log To Console              *********************
+    Log To Console              retrurn_code = ${rc}
+    Log To Console              output = ${output}
+    Should Be Equal As Integers    ${rc}    0
+    Should Contain    ${output}    cond-api
+
+Check ConductorController Docker Container
+    [Documentation]    It checks zookeeper docker container is running
+    ${rc}    ${output}=    Run and Return RC and Output    docker ps
+    Log To Console              *********************
+    Log To Console              retrurn_code = ${rc}
+    Log To Console              output = ${output}
+    Should Be Equal As Integers    ${rc}    0
+    Should Contain    ${output}    cond-cont
+
+Check ConductorSolver Docker Container
+    [Documentation]    It checks tomcat docker container is running
+    ${rc}    ${output}=    Run and Return RC and Output    docker ps
+    Log To Console              *********************
+    Log To Console              retrurn_code = ${rc}
+    Log To Console              output = ${output}
+    Should Be Equal As Integers    ${rc}    0
+    Should Contain    ${output}    cond-solv
+
+Check ConductorReservation War Docker Container
+    [Documentation]    It checks music.war docker container is running
+    ${rc}    ${output}=    Run and Return RC and Output    docker ps
+    Log To Console              *********************
+    Log To Console              retrurn_code = ${rc}
+    Log To Console              output = ${output}
+    Should Be Equal As Integers    ${rc}    0
+    Should Contain    ${output}    cond-resv
+
 Get Root Url
     [Documentation]    It sends a REST GET request to root url
     Create Session   optf-cond            ${COND_HOSTNAME}:${COND_PORT}
     &{headers}=      Create Dictionary    Content-Type=application/json  Accept=application/json
-    ${resp}=         Get Request        optf-cond   /v1/plans/     headers=${headers}
+    ${resp}=         Get Request        optf-cond   /     headers=${headers}
     Log To Console              *********************
     Log To Console              response = ${resp}
     Log To Console              body = ${resp.text}
