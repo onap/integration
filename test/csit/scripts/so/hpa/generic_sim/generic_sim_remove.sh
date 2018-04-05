ba5d1032fb19b5a10f3e59411955a8dc68a41de6
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2018-04-04 17:58:19 -0700

    [WIP. DO NOT MERGE] CSIT tests for HPA in SO
    This patch adds CSIT tests for HPA in SO.
    
    Change-Id: I773c835457e5112812d4d32ee72f8b16876b3c98
    Issue-ID: SO-555
    Signed-off-by: Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>

---
+++ test/csit/scripts/so/hpa/generic_sim/generic_sim_remove.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -x
+
+# cp /tmp/mso-docker.json ${WORKSPACE}/test/csit/scripts/so/chef-config/mso-docker.json
+rm -rf logs/*
+docker stop generic_sim
+docker rm generic_sim
+docker rmi generic_sim
