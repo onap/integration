f5b6edf04f07ff98c41a0d25e5639e8844d9377c
Gary Wu <gary.i.wu@huawei.com>
2018-04-06 10:56:50 -0700

    Enforce consistent sort order across environments
    Change-Id: If4b753ca344fecfaf32b3a6c498edb58c4f6a0f6
    Issue-ID: INT-124
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ version-manifest/src/main/scripts/check-sorted.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+LC_ALL=C sort -c $1
+retval=$?
+if [ $retval -ne 0 ]; then
+    echo
+    echo "ERROR: $1 is not properly sorted.  Please sort it with the following commands:"
+    echo
+    echo "  LC_ALL=C sort < $1 > $1.tmp"
+    echo "  mv $1.tmp $1"
+    echo
+fi
+exit $retval
