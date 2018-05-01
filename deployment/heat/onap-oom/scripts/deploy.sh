de82727db0394d7d3d9a738c1edc75ca8dcad7f5
Gary Wu <gary.i.wu@huawei.com>
2018-05-01 15:59:28 -0700

    Run OOM with 7 Kubernetes host VMs
    Also remove obsolete HEAT parameters.
    
    Change-Id: Ifb644cc354b8dc4dc8a8c39023b2016eaf84c7da
    Issue-ID: INT-381
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- deployment/heat/onap-oom/scripts/deploy.sh
+++ deployment/heat/onap-oom/scripts/deploy.sh
@@ -21,31 +21,30 @@ for n in $(seq 1 5); do
 
     for i in $(seq 1 30); do
 	sleep 30
-	K8S_IP=$(openstack stack output show onap-oom k8s_vm_ip -c output_value -f value)
 	RANCHER_IP=$(openstack stack output show onap-oom rancher_vm_ip -c output_value -f value)
-	timeout 1 ping -c 1 "$K8S_IP" && timeout 1 ping -c 1 "$RANCHER_IP" && break
+	timeout 1 ping -c 1 "$RANCHER_IP" && break
     done
 
-    timeout 1 ping -c 1 "$K8S_IP" && timeout 1 ping -c 1 "$RANCHER_IP" && break
+    timeout 1 ping -c 1 "$RANCHER_IP" && break
 
-    echo Error: OpenStack infrastructure issue: unable to reach both rancher "$RANCHER_IP" and k8s "$K8S_IP"
+    echo Error: OpenStack infrastructure issue: unable to reach rancher "$RANCHER_IP"
     sleep 10
 done
 
-if ! timeout 1 ping -c 1 "$K8S_IP" || ! timeout 1 ping -c 1 "$RANCHER_IP"; then
+if ! timeout 1 ping -c 1 "$RANCHER_IP"; then
     exit 2
 fi
 
-ssh-keygen -R $K8S_IP
+ssh-keygen -R $RANCHER_IP
 for n in $(seq 1 10); do
-    timeout 15m ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$K8S_IP  'sudo su -l root -c "/root/oom/kubernetes/robot/ete-k8s.sh onap health"'
+    timeout 15m ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$RANCHER_IP  'sudo su -l root -c "/root/oom/kubernetes/robot/ete-k8s.sh onap health"'
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
   	break
     fi
     sleep 15m
 done
-ROBOT_POD=$(ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$K8S_IP 'sudo su -c "kubectl --namespace onap get pods"' | grep robot | sed 's/ .*//')
-LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$K8S_IP "sudo su -c \"kubectl exec $ROBOT_POD --namespace onap -- ls -1t /share/logs | head -1\"")
-wget --user=robot --password=robot -r -np -nH --cut-dirs=2 -R "index.html*" -P $WORKSPACE/archives/ http://$K8S_IP:30209/logs/$LOG_DIR/
+ROBOT_POD=$(ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$RANCHER_IP 'sudo su -c "kubectl --namespace onap get pods"' | grep robot | sed 's/ .*//')
+LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ~/.ssh/onap_key ubuntu@$RANCHER_IP "sudo su -c \"kubectl exec $ROBOT_POD --namespace onap -- ls -1t /share/logs | head -1\"")
+wget --user=robot --password=robot -r -np -nH --cut-dirs=2 -R "index.html*" -P $WORKSPACE/archives/ http://$RANCHER_IP:30209/logs/$LOG_DIR/
 exit 0
