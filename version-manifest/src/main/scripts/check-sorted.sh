585971a297bb9086057ea3cddeb6dcfabeb6e44f
Gary Wu <gary.i.wu@huawei.com>
2018-05-03 17:06:06 -0700

    Verify that docker manifest images exist in nexus3
    Add enforcement check to ensure that all docker images
    specified in the manifest can actually be found on nexus3.
    
    Change-Id: I49ebc60a50cbb9f36f6e06d2d69c39cfeeacfcde
    Issue-ID: INT-488
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- version-manifest/src/main/scripts/check-sorted.sh
+++ version-manifest/src/main/scripts/check-sorted.sh
@@ -3,7 +3,7 @@ LC_ALL=C sort -c $1
 retval=$?
 if [ $retval -ne 0 ]; then
     echo
-    echo "ERROR: $1 is not properly sorted.  Please sort it with the following commands:"
+    echo "[ERROR] $1 is not properly sorted.  Please sort it with the following commands:"
     echo
     echo "  LC_ALL=C sort < $1 > $1.tmp"
     echo "  mv $1.tmp $1"
