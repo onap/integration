e003192163f37c339cec20a144f3b74aad3a6ecb
Gary Wu <gary.i.wu@huawei.com>
2018-05-21 21:53:42 -0700

    Clean up HEAT health check script
    Change-Id: I4215c561149bd4d6d21e37f23fae5c8dd041317a
    Issue-ID: INT-381
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- test/ete/scripts/run-healthcheck.sh
+++ test/ete/scripts/run-healthcheck.sh
@@ -19,10 +19,12 @@ fi
 
 ssh-keygen -R ${ROBOT_IP}
 
+set +x
 timeout 15m ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "OS_PROJECT_ID=$OS_PROJECT_ID OS_USERNAME=$OS_USERNAME OS_PASSWORD=$OS_PASSWORD bash -s" < ./remote/run-robot.sh
 RESULT=$?
+set -x
 
-LOG_DIR=$(ssh -o StrictHostKeychecking=no -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | head -1")
+LOG_DIR=$(ssh -i ${SSH_KEY} root@${ROBOT_IP} "ls -1t /opt/eteshare/logs | grep health | head -1")
 echo "Browse Robot results at http://${ROBOT_IP}:88/logs/${LOG_DIR}/"
-rsync -e "ssh -i ${SSH_KEY}" -avPz root@${ROBOT_IP}:/opt/eteshare/logs/${LOG_DIR}/ $WORKSPACE/archives/
+rsync -e "ssh -i ${SSH_KEY}" -avtz root@${ROBOT_IP}:/opt/eteshare/logs/${LOG_DIR}/ $WORKSPACE/archives/
 exit $RESULT
