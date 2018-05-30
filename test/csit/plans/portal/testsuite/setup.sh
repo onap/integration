63e8083487abe57f2a6301941dc121f548a67065
CHINTAMANI <sireesh.ch@in.ibm.com>
2018-05-30 17:46:24 -0400

    Updating CSIT Env file
    Change-Id: Ife1dc18c7315764d39cd300a6934f1b056d37fef
    Issue-ID: PORTAL-173
    Signed-off-by: Sireesh <sireesh.ch@in.ibm.com>

--- test/csit/plans/portal/testsuite/setup.sh
+++ test/csit/plans/portal/testsuite/setup.sh
@@ -177,8 +177,11 @@ export HOST_IP=${HOST_IP}
 
 docker logs deliveries_portal-db_1
 docker logs deliveries_portal-app_1
+docker logs deliveries_portal-sdk_1
 docker logs deliveries_portal-wms_1
 
 
-
+tail -500 $LOGS_DIR/onapportal/error.log
+tail -500 $LOGS_DIR/onapportal/application.log
+tail -500 $LOGS_DIR/onapportal/debug.log
 
