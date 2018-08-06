bbdad5e84148324f4ca1473ceb647b11cdd31332
Filip Krzywka <filip.krzywka@nokia.com>
2018-08-06 10:37:39 +0200

    Start xNF simulators in HV-VES test suite
    - keywords for containers start-up and shutdown
    - keyword for sending messages from simulators to HV-VES
    - aligned DCAE app assertion with sent messages amount to confirm
    that it actually consumes messages from kafka
    
    Change-Id: I123774949925c9e97c14ed5368e8cef64a7f23b5
    Issue-ID: DCAEGEN2-687
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- test/csit/tests/dcaegen2-collectors-hv-ves/testcases/libraries/VesHvContainersUtilsLibrary.py
+++ test/csit/tests/dcaegen2-collectors-hv-ves/testcases/libraries/VesHvContainersUtilsLibrary.py
@@ -2,6 +2,10 @@ from time import time
 
 from robot.api import logger
 import os.path
+import docker
+from io import BytesIO
+from os.path import basename
+from tarfile import TarFile, TarInfo
 
 LOCALHOST = "localhost"
 
@@ -13,6 +17,14 @@ class VesHvContainersUtilsLibrary:
             method,
             self.get_instance_address(image_name, port)
         )
+
+    def get_xnf_sim_api_access_url(self, method, host):
+        if is_running_inside_docker():
+            return self.create_url(method, host)
+        else:
+            logger.info("File `/.dockerenv` not found. Assuming local environment and using localhost.")
+            port_from_container_name = str(host)[-4:]
+            return self.create_url(method, LOCALHOST + ":" + port_from_container_name)
 
     def get_dcae_app_api_access_url(self, method, image_name, port):
         return self.create_url(
@@ -32,3 +44,26 @@ class VesHvContainersUtilsLibrary:
 
 def is_running_inside_docker():
     return os.path.isfile("/.dockerenv")
+
+def copy_to_container(container_id, filepaths, path='/etc/ves-hv'):
+    with create_archive(filepaths) as archive:
+        docker.APIClient('unix:///var/run/docker.sock') \
+            .put_archive(container=container_id, path=(path), data=archive)
+
+
+def create_archive(filepaths):
+    tarstream = BytesIO()
+    tarfile = TarFile(fileobj=tarstream, mode='w')
+    for filepath in filepaths:
+        file = open(filepath, 'r')
+        file_data = file.read()
+
+        tarinfo = TarInfo(name=basename(file.name))
+        tarinfo.size = len(file_data)
+        tarinfo.mtime = time()
+
+        tarfile.addfile(tarinfo, BytesIO(file_data))
+
+    tarfile.close()
+    tarstream.seek(0)
+    return tarstream
