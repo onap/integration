dd764a140059b1803b9b84b51e0fec7867db8c07
Filip Krzywka <filip.krzywka@nokia.com>
2018-08-06 09:31:40 +0200

    Setup IT environment for HV-VES component
    - containers start-up and shutdown
    - containers logs copied to archives
    - robot tests started successfully
    
    Change-Id:Idfa24a8d6a2f41cc8e1374eb2bf2b5967f533f47
    Issue-ID: DCAEGEN2-687
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

---
+++ test/csit/plans/dcaegen2/hv-ves-testsuites/setup.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+pip install docker
+
+COMPOSE_VERSION=1.22.0
+COMPOSE_LOCATION='/usr/local/bin/docker-compose'
+sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o ${COMPOSE_LOCATION}
+sudo chmod +x ${COMPOSE_LOCATION}
+
+
+echo "Removing not used docker networks"
+docker network prune -f
+
+export CONTAINERS_NETWORK=ves-hv-default
+echo "Creating network for containers: ${CONTAINERS_NETWORK}"
+docker network create ${CONTAINERS_NETWORK}
+
+cd ssl
+make FILE=client
+make FILE=server
+make FILE=invalid_client CA=invalid_trust
+cd ..
+
+export DOCKER_REGISTRY="nexus3.onap.org:10001"
+CURRENT_DIR=${PWD##*/}
+VES_HV_CONTAINER_NAME=ves-hv-collector
+
+# little race condition between container start-up and required files copying below
+docker-compose up -d
+
+COMPOSE_VES_HV_CONTAINER_NAME=${CURRENT_DIR}_${VES_HV_CONTAINER_NAME}_1
+echo "COPY tls authorization files to container: ${COMPOSE_VES_HV_CONTAINER_NAME}"
+docker cp ssl/. ${COMPOSE_VES_HV_CONTAINER_NAME}:/etc/ves-hv
+# race condition end
+
+
+export ROBOT_VARIABLES="--pythonpath ${WORKSPACE}/test/csit/tests/dcaegen2/hv-ves-testcases/libraries"
---
+++ test/csit/tests/dcaegen2/hv-ves-testcases/hv-ves.robot
@@ -0,0 +1,4 @@
+*** Test Cases ***
+Initial testcase
+    [Documentation]   Testing tests setup script
+    Log   Robot framework execution successful
