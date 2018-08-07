f09a89a68a509453403212ce2905d8f23d7c0d49
fujinhua <fu.jinhua@zte.com.cn>
2018-08-07 16:51:03 +0800

    Add db component when exec VFC CSIT
    Change-Id: I90b411f2cce47d2926ee35f867cdd7e628b167bc
    Issue-ID: INT-613
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- test/csit/plans/vfc-nfvo-lcm/sanity-check/setup.sh
+++ test/csit/plans/vfc-nfvo-lcm/sanity-check/setup.sh
@@ -32,6 +32,10 @@ docker run -d -p 80:80 -e CONSUL_IP=$MSB_CONSUL_IP -e SDCLIENT_IP=$MSB_DISCOVERY
 MSB_IAG_IP=`get-instance-ip.sh msb_internal_apigateway`
 echo MSB_IAG_IP=${MSB_IAG_IP}
 
+docker run -d -p 3306:3306 --name vfc-db -v /var/lib/mysql nexus3.onap.org:10001/onap/vfc/db
+VFC_DB_IP=`get-instance-ip.sh vfc-db`
+echo VFC_DB_IP=${VFC_DB_IP}
+
 # Wait for initialization(8500 Consul, 10081 Service Registration & Discovery, 80 api gateway)
 for i in {1..10}; do
     curl -sS -m 1 ${MSB_CONSUL_IP}:8500 && curl -sS -m 1 ${MSB_DISCOVERY_IP}:10081 && curl -sS -m 1 ${MSB_IAG_IP}:80 && break
@@ -39,12 +43,25 @@ for i in {1..10}; do
     sleep $i
 done
 
+# Wait for initialization(3306 DB)
+for i in {1..3}; do
+    curl -sS -m 1 ${VFC_DB_IP}:3306 && break
+    echo sleep $i
+    sleep $i
+done
+
+docker ps > 1.txt
+cat 1.txt
+echo "****************************"
+docker logs -f vfc-db > 2.txt &
+cat 2.txt
+
 # Need some time so service info can be synced from discovery to api gateway
 echo sleep 60
 sleep 60
 
 # start vfc-nslcm
-docker run -d --name vfc-nslcm -v /var/lib/mysql -e MSB_ADDR=${MSB_IAG_IP}:80 nexus3.onap.org:10001/onap/vfc/nslcm
+docker run -d --name vfc-nslcm -v /var/lib/mysql -e MSB_ADDR=${MSB_IAG_IP}:80 -e MYSQL_ADDR=${VFC_DB_IP}:3306 nexus3.onap.org:10001/onap/vfc/nslcm
 NSLCM_IP=`get-instance-ip.sh vfc-nslcm`
 
 # Wait for initialization
