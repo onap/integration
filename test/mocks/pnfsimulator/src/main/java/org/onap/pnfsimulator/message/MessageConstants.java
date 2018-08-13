a48a9cfcf768053d331f7b0cc273c87c49989d37
Marcin Migdal <marcin.migdal@nokia.com>
2018-08-13 16:51:51 +0200

    Use values according to Nokia proposal
    Change-Id: I7eface70fef2e0687e42d30e1c05af20761727de
    Issue-ID: INT-622
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageConstants.java
+++ test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageConstants.java
@@ -2,34 +2,42 @@ package org.onap.pnfsimulator.message;
 
 public final class MessageConstants {
 
-    public static final String EVENT = "event";
-    public static final String DOMAIN = "domain";
-    public static final String EVENT_ID = "eventId";
-    public static final String EVENT_TYPE = "eventType";
-    public static final String LAST_EPOCH_MICROSEC = "lastEpochMicrosec";
-    public static final String PRIORITY = "priority";
-    public static final String SEQUENCE = "sequence";
-    public static final String START_EPOCH_MICROSEC = "startEpochMicrosec";
-    public static final String INTERNAL_HEADER_FIELDS = "internalHeaderFields";
-    public static final String VERSION = "version";
-    public static final String OTHER_FIELDS_VERSION = "otherFieldsVersion";
-    public static final String PNF_LAST_SERVICE_DATE = "pnfLastServiceDate";
-    public static final String PNF_MANUFACTURE_DATE = "pnfManufactureDate";
+    static final String EVENT = "event";
+    static final String DOMAIN = "domain";
+    static final String EVENT_ID = "eventId";
+    static final String EVENT_TYPE = "eventType";
+    static final String LAST_EPOCH_MICROSEC = "lastEpochMicrosec";
+    static final String PRIORITY = "priority";
+    static final String SEQUENCE = "sequence";
+    static final String START_EPOCH_MICROSEC = "startEpochMicrosec";
+    static final String INTERNAL_HEADER_FIELDS = "internalHeaderFields";
+    static final String VERSION = "version";
+    static final String OTHER_FIELDS_VERSION = "otherFieldsVersion";
+    static final String PNF_LAST_SERVICE_DATE = "pnfLastServiceDate";
+    static final String PNF_MANUFACTURE_DATE = "pnfManufactureDate";
 
-    public static final String SIMULATOR_PARAMS_CONTAINER = "simulatorParams";
-    public static final String MESSAGE_PARAMS_CONTAINER = "messageParams";
+    static final String SIMULATOR_PARAMS_CONTAINER = "simulatorParams";
+    static final String MESSAGE_PARAMS_CONTAINER = "messageParams";
 
-    // mandatory
-    public static final String PNF_OAM_IPV4_ADDRESS = "pnfOamIpv4Address";
-    public static final String PNF_OAM_IPV6_ADDRESS = "pnfOamIpv6Address";
-    public static final String PNF_SERIAL_NUMBER = "pnfSerialNumber";
-    public static final String PNF_VENDOR_NAME = "pnfVendorName";
-    public static final String VES_SERVER_URL = "vesServerUrl";
-    public static final String PNF_PREFIX = "pnf";
-    public static final String COMMON_EVENT_HEADER = "commonEventHeader";
-    public static final String OTHER_FIELDS = "otherFields";
-    public static final String TEST_DURATION = "testDuration";
-    public static final String MESSAGE_INTERVAL = "messageInterval";
+    // mandatory used in json file, but not in java logic
+    //public static final String PNF_OAM_IPV4_ADDRESS = "pnfOamIpv4Address";
+    //public static final String PNF_OAM_IPV6_ADDRESS = "pnfOamIpv6Address";
+    //public static final String PNF_SERIAL_NUMBER = "pnfSerialNumber";
+    //public static final String PNF_VENDOR_NAME = "pnfVendorName";
+    static final String VES_SERVER_URL = "vesServerUrl";
+    static final String PNF_PREFIX = "pnf";
+    static final String COMMON_EVENT_HEADER = "commonEventHeader";
+    static final String OTHER_FIELDS = "otherFields";
+    static final String TEST_DURATION = "testDuration";
+    static final String MESSAGE_INTERVAL = "messageInterval";
+
+    //===============================================================
+    //constant values
+    static final String PNF_REGISTRATION ="pnfRegistration";
+    static final String PRIORITY_NORMAL = "Normal";
+    static final float VERSION_NUMBER = 3.0f;
+    static final int SEQUENCE_NUMBER = 0;
+    static final int OTHER_FIELDS_VERSION_VALUE = 1;
 
     private MessageConstants() {
     }
--- test/mocks/pnfsimulator/src/test/java/org/onap/pnfsimulator/message/MessageProviderTest.java
+++ test/mocks/pnfsimulator/src/test/java/org/onap/pnfsimulator/message/MessageProviderTest.java
@@ -4,22 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.onap.pnfsimulator.message.MessageConstants.COMMON_EVENT_HEADER;
-import static org.onap.pnfsimulator.message.MessageConstants.DOMAIN;
 import static org.onap.pnfsimulator.message.MessageConstants.EVENT;
-import static org.onap.pnfsimulator.message.MessageConstants.EVENT_ID;
-import static org.onap.pnfsimulator.message.MessageConstants.EVENT_TYPE;
-import static org.onap.pnfsimulator.message.MessageConstants.INTERNAL_HEADER_FIELDS;
-import static org.onap.pnfsimulator.message.MessageConstants.LAST_EPOCH_MICROSEC;
 import static org.onap.pnfsimulator.message.MessageConstants.OTHER_FIELDS;
-import static org.onap.pnfsimulator.message.MessageConstants.OTHER_FIELDS_VERSION;
-import static org.onap.pnfsimulator.message.MessageConstants.PNF_LAST_SERVICE_DATE;
-import static org.onap.pnfsimulator.message.MessageConstants.PNF_MANUFACTURE_DATE;
-import static org.onap.pnfsimulator.message.MessageConstants.PRIORITY;
-import static org.onap.pnfsimulator.message.MessageConstants.SEQUENCE;
-import static org.onap.pnfsimulator.message.MessageConstants.START_EPOCH_MICROSEC;
-import static org.onap.pnfsimulator.message.MessageConstants.VERSION;
-
-import java.util.UUID;
 import org.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -51,8 +37,8 @@ public class MessageProviderTest {
         JSONObject commonEventHeader = event.getJSONObject(COMMON_EVENT_HEADER);
         JSONObject otherFields = event.getJSONObject(OTHER_FIELDS);
 
-        JSONObject expectedCommonEventHeader = generateConstantCommonEventHeader();
-        JSONObject expectedOtherFields = generateConstantOtherFields();
+        JSONObject expectedCommonEventHeader = JSONObjectFactory.generateConstantCommonEventHeader();
+        JSONObject expectedOtherFields = JSONObjectFactory.generateConstantOtherFields();
 
         expectedCommonEventHeader
             .toMap()
@@ -79,35 +65,6 @@ public class MessageProviderTest {
         assertEquals("val1", commonEventHeader.getString("key1"));
         assertEquals("val2", commonEventHeader.getString("key2"));
         assertEquals("val4", commonEventHeader.getString("key4"));
-    }
-
-
-    private JSONObject generateConstantCommonEventHeader() {
-
-        JSONObject commonEventHeader = new JSONObject();
-        long timestamp = System.currentTimeMillis();
-
-        commonEventHeader.put(DOMAIN, "other");
-        commonEventHeader.put(EVENT_ID, UUID.randomUUID() + "-reg");
-        commonEventHeader.put(EVENT_TYPE, "pnfRegistration");
-        commonEventHeader.put(LAST_EPOCH_MICROSEC, timestamp);
-        commonEventHeader.put(PRIORITY, "Normal");
-        commonEventHeader.put(SEQUENCE, 0);
-        commonEventHeader.put(START_EPOCH_MICROSEC, timestamp);
-        commonEventHeader.put(INTERNAL_HEADER_FIELDS, new JSONObject());
-        commonEventHeader.put(VERSION, 3);
-
-        return commonEventHeader;
-    }
-
-    private JSONObject generateConstantOtherFields() {
-        JSONObject otherFields = new JSONObject();
-
-        otherFields.put(OTHER_FIELDS_VERSION, 1);
-        otherFields.put(PNF_LAST_SERVICE_DATE, 1517206400);
-        otherFields.put(PNF_MANUFACTURE_DATE, 1516406400);
-
-        return otherFields;
     }
 
 }
