6e5e2818a7325b19e0757a4c4e11efd0f2798fb3
Marcin Migdal <marcin.migdal@nokia.com>
2018-08-14 17:23:10 +0200

    Preparation for INT-622 code
    Change-Id: I9e536254a5eeb06864792c6f67aa0c3e2029c24f
    Issue-ID: INT-622
    Signed-off-by: Marcin Migdal <marcin.migdal@nokia.com>

--- test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageProvider.java
+++ test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageProvider.java
@@ -1,24 +1,10 @@
 package org.onap.pnfsimulator.message;
 
 import static org.onap.pnfsimulator.message.MessageConstants.COMMON_EVENT_HEADER;
-import static org.onap.pnfsimulator.message.MessageConstants.DOMAIN;
 import static org.onap.pnfsimulator.message.MessageConstants.EVENT;
-import static org.onap.pnfsimulator.message.MessageConstants.EVENT_ID;
-import static org.onap.pnfsimulator.message.MessageConstants.EVENT_TYPE;
-import static org.onap.pnfsimulator.message.MessageConstants.INTERNAL_HEADER_FIELDS;
-import static org.onap.pnfsimulator.message.MessageConstants.LAST_EPOCH_MICROSEC;
 import static org.onap.pnfsimulator.message.MessageConstants.OTHER_FIELDS;
-import static org.onap.pnfsimulator.message.MessageConstants.OTHER_FIELDS_VERSION;
-import static org.onap.pnfsimulator.message.MessageConstants.PNF_LAST_SERVICE_DATE;
-import static org.onap.pnfsimulator.message.MessageConstants.PNF_MANUFACTURE_DATE;
 import static org.onap.pnfsimulator.message.MessageConstants.PNF_PREFIX;
-import static org.onap.pnfsimulator.message.MessageConstants.PRIORITY;
-import static org.onap.pnfsimulator.message.MessageConstants.SEQUENCE;
-import static org.onap.pnfsimulator.message.MessageConstants.START_EPOCH_MICROSEC;
-import static org.onap.pnfsimulator.message.MessageConstants.VERSION;
-
 import java.util.Map;
-import java.util.UUID;
 import org.json.JSONObject;
 
 public class MessageProvider {
@@ -31,8 +17,8 @@ public class MessageProvider {
 
         Map<String, Object> paramsMap = params.toMap();
         JSONObject root = new JSONObject();
-        JSONObject commonEventHeader = generateConstantCommonEventHeader();
-        JSONObject otherFields = generateConstantOtherFields();
+        JSONObject commonEventHeader = JSONObjectFactory.generateConstantCommonEventHeader();
+        JSONObject otherFields = JSONObjectFactory.generateConstantOtherFields();
 
         paramsMap.forEach((key, value) -> {
 
@@ -50,32 +36,4 @@ public class MessageProvider {
         return root;
     }
 
-    private JSONObject generateConstantCommonEventHeader() {
-
-        JSONObject commonEventHeader = new JSONObject();
-        long timestamp = System.currentTimeMillis();
-
-        commonEventHeader.put(DOMAIN, "other");
-        commonEventHeader.put(EVENT_ID, UUID.randomUUID() + "-reg");
-        commonEventHeader.put(EVENT_TYPE, "pnfRegistration");
-        commonEventHeader.put(LAST_EPOCH_MICROSEC, timestamp);
-        commonEventHeader.put(PRIORITY, "Normal");
-        commonEventHeader.put(SEQUENCE, 0);
-        commonEventHeader.put(START_EPOCH_MICROSEC, timestamp);
-        commonEventHeader.put(INTERNAL_HEADER_FIELDS, new JSONObject());
-        commonEventHeader.put(VERSION, 3);
-
-        return commonEventHeader;
-    }
-
-    private JSONObject generateConstantOtherFields() {
-
-        JSONObject otherFields = new JSONObject();
-
-        otherFields.put(OTHER_FIELDS_VERSION, 1);
-        otherFields.put(PNF_LAST_SERVICE_DATE, System.currentTimeMillis());
-        otherFields.put(PNF_MANUFACTURE_DATE, System.currentTimeMillis());
-
-        return otherFields;
-    }
 }
