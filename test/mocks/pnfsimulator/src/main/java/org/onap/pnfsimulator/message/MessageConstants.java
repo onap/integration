498fdd1886e3958864fa49af6a8f55c9f0b4d7da
micdzied <michal.1.dziedzic@nokia.com>
2018-08-17 09:58:00 +0200

    update according to new schema
    Change-Id: I7fbe1b765be33e3c7734786cd77625360a0e1f92
    Issue-ID: INT-628
    Signed-off-by: micdzied <michal.1.dziedzic@nokia.com>

--- test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageConstants.java
+++ test/mocks/pnfsimulator/src/main/java/org/onap/pnfsimulator/message/MessageConstants.java
@@ -14,9 +14,10 @@ public final class MessageConstants {
     static final String START_EPOCH_MICROSEC = "startEpochMicrosec";
     static final String INTERNAL_HEADER_FIELDS = "internalHeaderFields";
     static final String VERSION = "version";
-    static final String OTHER_FIELDS_VERSION = "otherFieldsVersion";
-    static final String PNF_LAST_SERVICE_DATE = "pnfLastServiceDate";
-    static final String PNF_MANUFACTURE_DATE = "pnfManufactureDate";
+    static final String PNF_REGISTRATION_FIELDS_VERSION = "pnfRegistrationFieldsVersion";
+    static final String PNF_LAST_SERVICE_DATE = "lastServiceDate";
+    static final String PNF_MANUFACTURE_DATE = "manufactureDate";
+    static final String VES_EVENT_LISTENER_VERSION = "vesEventListenerVersion";
 
     // mandatory used in json file, but not in java logic
     //public static final String PNF_OAM_IPV4_ADDRESS = "pnfOamIpv4Address";
@@ -26,18 +27,19 @@ public final class MessageConstants {
     public static final String VES_SERVER_URL = "vesServerUrl";
     public static final String TEST_DURATION = "testDuration";
     public static final String MESSAGE_INTERVAL = "messageInterval";
-    static final String PNF_PREFIX = "pnf";
+    static final String PNF_PREFIX = "pnf_";
     static final String COMMON_EVENT_HEADER = "commonEventHeader";
-    static final String OTHER_FIELDS = "otherFields";
+    static final String PNF_REGISTRATION_FIELDS = "pnfRegistrationFields";
 
 
     //===============================================================
     //constant values
     static final String PNF_REGISTRATION ="pnfRegistration";
     static final String PRIORITY_NORMAL = "Normal";
-    static final float VERSION_NUMBER = 3.0f;
+    static final String VERSION_NUMBER = "4.0.1";
+    static final String VES_EVENT_LISTENER_VERSION_NUMBER = "7.0.1";
+    static final String PNF_REGISTRATION_FIELDS_VERSION_VALUE = "2.0";
     static final int SEQUENCE_NUMBER = 0;
-    static final int OTHER_FIELDS_VERSION_VALUE = 1;
 
     private MessageConstants() {
     }
