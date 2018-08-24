27f63596b868bfb0e08e7b2b73f5caf1da2e9976
Vikram Potturi <vp9004@att.com>
2018-08-24 14:40:09 -0400

    fixed variable Autharization
    update to print out tomcat logs for troubleshooting
    More Logging
    
    Change-Id: I1273ffa3de72587c2f7040593b7208a1a1485ca0
    Issue-ID: MUSIC-100
    Signed-off-by: Vikram Potturi <vp9004@att.com>
    Signed-off-by: Nelson,Thomas(tn1381)(arthurdent3) <tn1381@att.com>

--- test/csit/plans/music/music-test-plan/setup.sh
+++ test/csit/plans/music/music-test-plan/setup.sh
@@ -77,6 +77,13 @@ echo "TOMCAT_IP=${TOMCAT_IP}"
 
 ${WORKSPACE}/test/csit/scripts/music/music-scripts/wait_for_port.sh ${TOMCAT_IP} 8080
 
+sleep 10;
+echo "get the tomcat logs to make sure its running music properly"
+docker logs music-tomcat
+echo "===== MUSIC Log ===================="
+tail -100 $MUSIC_LOGS/MUSIC/music.log
+echo "===== MUSIC error log =================="
+tail -100 $MUSIC_LOGS/MUSIC/error.log
 
 echo "inspect docker things for tracing purpose"
 docker inspect music-db
