2399355255249d862f6faf3c058abb11e0c826cf
Jakub Dudycz <jakub.dudycz@nokia.com>
2018-09-03 14:15:55 +0200

    Define simple health check
    Change-Id: Ideac561fb0d30e48ecf0362e73145a93b0c30367
    Issue-ID: DCAEGEN2-659
    Signed-off-by: Jakub Dudycz <jakub.dudycz@nokia.com>
    Signed-off-by: Piotr Jaszczyk <piotr.jaszczyk@nokia.com>

--- test/csit/plans/dcaegen2-collectors-hv-ves/testsuites/setup.sh
+++ test/csit/plans/dcaegen2-collectors-hv-ves/testsuites/setup.sh
@@ -2,13 +2,13 @@
 
 set -euo pipefail
 
-#pip uninstall -y docker-py
-#pip install docker
-#
-#COMPOSE_VERSION=1.22.0
-#COMPOSE_LOCATION='/usr/local/bin/docker-compose'
-#sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o ${COMPOSE_LOCATION}
-#sudo chmod +x ${COMPOSE_LOCATION}
+pip uninstall -y docker-py
+pip install docker
+
+COMPOSE_VERSION=1.22.0
+COMPOSE_LOCATION='/usr/local/bin/docker-compose'
+sudo curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m) -o ${COMPOSE_LOCATION}
+sudo chmod +x ${COMPOSE_LOCATION}
 
 
 echo "Removing not used docker networks"
