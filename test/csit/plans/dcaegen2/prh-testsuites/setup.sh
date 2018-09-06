0a880ca0b0b0650983f05a804a2a6de48b44e131
Mariusz Wagner <mariusz.wagner@nokia.com>
2018-09-06 12:48:32 +0200

    Fix tests and updated docker files
    - fix tests in Jenkins
    - update docker files (reduce image size)
    
    Issue-ID: INT-647
    
    Change-Id: I2b0e541bc962b68d4e68c592c161ce91ff698d34
    Signed-off-by: Mariusz Wagner <mariusz.wagner@nokia.com>

--- test/csit/plans/dcaegen2/prh-testsuites/setup.sh
+++ test/csit/plans/dcaegen2/prh-testsuites/setup.sh
@@ -8,25 +8,8 @@ export AAI_SIMULATOR="aai_simulator"
 
 cd ${WORKSPACE}/test/csit/tests/dcaegen2/prh-testcases/resources/
 
-docker login -u docker -p docker nexus3.onap.org:10001
-pip uninstall -y docker-py
-pip uninstall -y docker
-pip install -U docker
+pip install docker --upgrade
 docker-compose up -d --build
-
-# Wait for initialization of Docker containers
-for i in {1..10}; do
-	if [ $(docker inspect --format '{{ .State.Running }}' ${PRH_SERVICE}) ] && \
-	  [ $(docker inspect --format '{{ .State.Running }}' ${DMAAP_SIMULATOR}) ] && \
-	  [ $(docker inspect --format '{{ .State.Running }}' ${AAI_SIMULATOR}) ]
-	then
-		echo "dmaap_simulator, aai_simulator and prh services are running"
-		break    		
-	else 
-		echo sleep ${i}
-		sleep ${i}
-	fi
-done
 
 PRH_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${PRH_SERVICE})
 DMAAP_SIMULATOR_IP=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ${DMAAP_SIMULATOR})
@@ -46,13 +29,6 @@ for i in {1..10}; do
     echo sleep ${i}
     sleep ${i}
 done
-
-docker stop prh
-docker cp prh:/config/prh_endpoints.json ${WORKDIR}
-sed -i -e 's/"dmaapHostName":.*/"dmaapHostName": "'${DMAAP_SIMULATOR_IP}'",/g' ${WORKDIR}/prh_endpoints.json
-sed -i -e 's/"aaiHost":.*/"aaiHost": "'${AAI_SIMULATOR_IP}'",/g' ${WORKDIR}/prh_endpoints.json
-docker cp ${WORKDIR}/prh_endpoints.json prh:/config/
-docker start prh
 
 # #Pass any variables required by Robot test suites in ROBOT_VARIABLES
 ROBOT_VARIABLES="-v DMAAP_SIMULATOR:${DMAAP_SIMULATOR_IP}:2222 -v AAI_SIMULATOR:${AAI_SIMULATOR_IP}:3333 -v PRH:${PRH_IP}:8100"
---
+++ test/csit/tests/dcaegen2/prh-testcases/__init__.robot
@@ -0,0 +1,2 @@
+*** Settings ***
+Documentation    Integration - PRH suite
