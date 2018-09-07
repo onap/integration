337bc73ca43ac7f6b2a8368ebdb0dc8a008a9fe8
rajendrajaiswal <rajendra.jaiswal@ericsson.com>
2018-09-07 15:23:18 +0100

    Integration Test Cases for 5G Bulk PM
    Change-Id: Ibec3e9726354b8f3818084b03f61c0e9aed09df2
    Issue-ID: INT-649
    Signed-off-by: Rajendra Jaiswal <rajendra.jaiswal@ericsson.com>

---
+++ test/csit/tests/dcae-bulkpm/testcases/resources/VesLibrary.py
@@ -0,0 +1,26 @@
+'''
+Created on Aug 18, 2017
+
+@author: sw6830
+'''
+from robot.api import logger
+from Queue import Queue
+import uuid, time, json, threading,os, platform, subprocess,paramiko
+
+class VesLibrary(object):
+    
+    def __init__(self):
+        pass 
+                    
+    def create_header_from_string(self, dictStr):
+        logger.info("Enter create_header_from_string: dictStr")
+        return dict(u.split("=") for u in dictStr.split(","))
+    
+    def Generate_UUID(self):
+        """generate a uuid"""
+        return uuid.uuid4()
+
+if __name__ == '__main__':
+    lib = VesLibrary()
+    time.sleep(100000)
+    
