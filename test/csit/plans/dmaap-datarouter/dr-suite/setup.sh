abb907ba4b16075664a6e6b62d83c0eb4b36111f
Conor Ward <conor.ward@ericsson.com>
2018-09-07 15:26:19 +0000

    Update CSIT tests to Pull Release Images
    Change-Id: Ib003559b6a197c12ca5ead915078e88131f4f535
    Signed-off-by: Conor Ward <conor.ward@ericsson.com>
    Issue-ID: DMAAP-737

--- test/csit/plans/dmaap-datarouter/dr-suite/setup.sh
+++ test/csit/plans/dmaap-datarouter/dr-suite/setup.sh
@@ -12,7 +12,7 @@ git pull
 cd $WORKSPACE/archives/dmaapdr/datarouter/docker-compose/
 
 # start DMaaP DR containers with docker compose and configuration from docker-compose.yml
-docker login -u docker -p docker nexus3.onap.org:10003
+docker login -u docker -p docker nexus3.onap.org:10001
 docker-compose up -d
 
 # Wait for initialization of Docker container for datarouter-node, datarouter-prov and mariadb
