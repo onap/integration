3b0185dd7d65c0ca1d6549f80f763ee8f44f640b
Dileep Ranganathan <dileep.ranganathan@intel.com>
2018-09-07 02:53:07 -0700

    Fix OOF CSIT using MUSIC 2.5.3 version
    Fix OOF CSIT using MUSIC 2.5.3 version
    
    Change-Id: Ic3b68b8cc6a59d8ba7d074d118281a549b523b35
    Issue-ID: OPTFRA-259
    Signed-off-by: Dileep Ranganathan <dileep.ranganathan@intel.com>

--- test/csit/scripts/optf-has/has/has_script.sh
+++ test/csit/scripts/optf-has/has/has_script.sh
@@ -70,25 +70,30 @@ echo "Onboard conductor into music"
 curl -vvvvv --noproxy "*" --request POST http://${MUSIC_IP}:8080/MUSIC/rest/v2/admin/onboardAppWithMusic -H "Content-Type: application/json" --data @${WORKSPACE}/test/csit/tests/optf-has/has/data/onboard.json
 
 docker run -d --name cond-cont -v ${COND_CONF}:/usr/local/bin/conductor.conf -v ${LOG_CONF}:/usr/local/bin/log.conf ${IMAGE_NAME}:latest python /usr/local/bin/conductor-controller --config-file=/usr/local/bin/conductor.conf
-sleep 2
+sleep 20
 docker run -d --name cond-api -p "8091:8091" -v ${COND_CONF}:/usr/local/bin/conductor.conf -v ${LOG_CONF}:/usr/local/bin/log.conf ${IMAGE_NAME}:latest python /usr/local/bin/conductor-api --port=8091 -- --config-file=/usr/local/bin/conductor.conf
-sleep 2
+sleep 20
 docker run -d --name cond-solv -v ${COND_CONF}:/usr/local/bin/conductor.conf -v ${LOG_CONF}:/usr/local/bin/log.conf ${IMAGE_NAME}:latest python /usr/local/bin/conductor-solver --config-file=/usr/local/bin/conductor.conf
-sleep 2
+sleep 20
 docker run -d --name cond-resv -v ${COND_CONF}:/usr/local/bin/conductor.conf -v ${LOG_CONF}:/usr/local/bin/log.conf ${IMAGE_NAME}:latest python /usr/local/bin/conductor-reservation --config-file=/usr/local/bin/conductor.conf
-sleep 2
+sleep 20
 docker run -d --name cond-data -v ${COND_CONF}:/usr/local/bin/conductor.conf -v ${LOG_CONF}:/usr/local/bin/log.conf -v ${CERT}:/usr/local/bin/cert.cer -v ${KEY}:/usr/local/bin/cert.key -v ${BUNDLE}:/usr/local/bin/cert.pem ${IMAGE_NAME}:latest python /usr/local/bin/conductor-data --config-file=/usr/local/bin/conductor.conf
-sleep 2
+sleep 20
 
 COND_IP=`docker inspect --format '{{ .NetworkSettings.Networks.bridge.IPAddress}}' cond-api`
 ${WORKSPACE}/test/csit/scripts/optf-has/has/wait_for_port.sh ${COND_IP} 8091
 
-echo "inspect docker things for tracing purpose"
-docker inspect cond-data
-docker inspect cond-cont
-docker inspect cond-api
-docker inspect cond-solv
-docker inspect cond-resv
+#echo "inspect docker things for tracing purpose"
+#docker inspect cond-data
+#docker inspect cond-cont
+#docker inspect cond-api
+#docker inspect cond-solv
+#docker inspect cond-resv
+
+echo "DEBUG DLOGS"
+docker logs cond-cont
+echo "DEBUG DLOGS"
+
 
 echo "dump music content just after conductor is started"
 docker exec music-db /usr/bin/nodetool status
