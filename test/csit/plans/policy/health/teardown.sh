3ff216847893a4686eafda50cc7d953ff1170da2
mmis <michael.morris@ericsson.com>
2018-09-10 22:27:34 +0100

    Adding apex-pdp csit test
    PLEASE DO NOT MERGE - NOT READY TO BE MERGED.
    
    Issue-ID: POLICY-1099
    Change-Id: I1a8845d6de0cb3f8c9c65b1a1ee8388965685e9c
    Signed-off-by: mmis <michael.morris@ericsson.com>

--- test/csit/plans/policy/health/teardown.sh
+++ test/csit/plans/policy/health/teardown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright 2017 AT&T Intellectual Property. All rights reserved.
+# Copyright 2016-2017 Huawei Technologies Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,22 +15,5 @@
 # limitations under the License.
 #
 
-function kill_instance() {
-local name=$1
-docker logs "${name}" >> "${WORKSPACE}"/archives/"${name}".log
-docker kill "${name}"
-docker rm -v "${name}"
-}
-
-mkdir -p "${WORKSPACE}"/archives
-
-kill_instance i-mock
-kill_instance drools
-kill_instance pdp
-kill_instance brmsgw
-kill_instance pap
-kill_instance nexus
-kill_instance mariadb
-
-rm -fr "${WORK_DIR}"
-
+docker logs apex >> "${WORKSPACE}"/archives/apex.log
+kill-instance.sh apexCsit
