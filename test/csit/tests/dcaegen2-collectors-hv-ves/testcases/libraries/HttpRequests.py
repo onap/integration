5c351dc937afe539dd0b13bf88b85dce1c233911
Filip Krzywka <filip.krzywka@nokia.com>
2018-09-10 11:39:59 +0200

    Add HTTP 202 status to valid client responses
    This is caused by changes made while resolving DCAEGEN2-739
    
    Change-Id: I91ee2018200c3fe8b19ea76ed95fb3e6827c4285
    Issue-ID: DCAEGEN2-761
    Signed-off-by: Filip Krzywka <filip.krzywka@nokia.com>

--- test/csit/tests/dcaegen2-collectors-hv-ves/testcases/libraries/HttpRequests.py
+++ test/csit/tests/dcaegen2-collectors-hv-ves/testcases/libraries/HttpRequests.py
@@ -1,12 +1,19 @@
 import requests
 from robot.api import logger
 
+valid_status_codes = [
+    requests.codes.ok,
+    requests.codes.accepted
+]
+
+
 def session_without_env():
     session = requests.Session()
     session.trust_env = False
     return session
 
+
 def checkStatusCode(status_code, server_name):
-    if status_code != 200:
+    if status_code not in valid_status_codes:
         logger.error("Response status code from " + server_name + ": " + str(status_code))
         raise (Exception(server_name + " returned status code " + status_code))
