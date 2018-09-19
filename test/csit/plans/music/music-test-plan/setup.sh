5d7183c56ee613b5fb3f03ff2f64a6a0ed646e86
Nelson,Thomas(tn1381)(arthurdent3) <tn1381@att.com>
2018-09-19 17:45:41 -0400

    Update CSIT Test to work with new Images.
    Minor changes
    Some cleanup and Output Changes
    
    Change-Id: Id16ce27e878f295a8adc4fd2736698f2ad471f17
    Issue-ID: MUSIC-138
    Signed-off-by: Nelson,Thomas(tn1381)(arthurdent3) <tn1381@att.com>

--- test/csit/plans/music/music-test-plan/setup.sh
+++ test/csit/plans/music/music-test-plan/setup.sh
@@ -18,24 +18,27 @@
 #
 # add here eventual scripts needed for music
 #
-echo "# music scripts calling";
+echo "########## music scripts calling ##########";
 source ${WORKSPACE}/test/csit/scripts/music/music-scripts/music_script.sh
 
 #
 # add here all the configuration steps eventually needed to be carried out for music CSIT testing
 #
-echo "# music configuration step";
-
-CASS_IMG=nexus3.onap.org:10001/onap/music/cassandra_music:latest
+echo "########## music configuration step ##########";
+CASS_IMG=nexus3.onap.org:10001/onap/music/cassandra_3_11:latest
+CASS_IMG_JOB=nexus3.onap.org:10001/onap/music/cassandra_job:latest
 TOMCAT_IMG=nexus3.onap.org:10001/library/tomcat:8.5
 ZK_IMG=nexus3.onap.org:10001/library/zookeeper:3.4
+BUSYBOX_IMG=nexus3.onap.org:10001/library/busybox:latest
 MUSIC_IMG=nexus3.onap.org:10001/onap/music/music:latest
+TT=10
 WORK_DIR=/tmp/music
 CASS_USERNAME=nelson24
 CASS_PASSWORD=winman123
 MUSIC_SOURCE_PROPERTIES=${WORKSPACE}/test/csit/scripts/music/music-properties
 MUSIC_PROPERTIES=/tmp/music/properties
 MUSIC_LOGS=/tmp/music/logs
+CQL_FILES=${WORKSPACE}/test/csit/scripts/music/cql
 mkdir -p ${MUSIC_PROPERTIES}
 mkdir -p ${MUSIC_LOGS}
 mkdir -p ${MUSIC_LOGS}/MUSIC
@@ -44,34 +47,64 @@ mkdir -p ${MUSIC_LOGS}/MUSIC
 cp ${MUSIC_SOURCE_PROPERTIES}/* ${WORK_DIR}/properties
 
 # Create Volume for mapping war file and tomcat
+echo "########## create music-vol ##########"
 docker volume create --name music-vol;
 
 # Create a network for all the containers to run in.
+echo "########## create music-net ##########"
 docker network create music-net;
 
 # Start Cassandra
+echo "########## Start Cassandra (music-db) ##########"
 docker run -d --name music-db --network music-net -p "7000:7000" -p "7001:7001" -p "7199:7199" -p "9042:9042" -p "9160:9160" -e CASSUSER=${CASS_USERNAME} -e CASSPASS=${CASS_PASSWORD} ${CASS_IMG};
 
 CASSA_IP=`docker inspect -f '{{ $network := index .NetworkSettings.Networks "music-net" }}{{ $network.IPAddress}}' music-db`
 echo "CASSANDRA_IP=${CASSA_IP}"
 ${WORKSPACE}/test/csit/scripts/optf-has/has/wait_for_port.sh ${CASSA_IP} 9042
 
+# See if cassandra is up.
+echo "########## Running Test to see if Cassandra is up ##########"
+docker run --name music-casstest --network music-net $BUSYBOX_IMG sh -c "until nc -z music-db 9042 && echo "success"; do echo 'No connection .. Sleeping for $TT seconds';sleep $TT; done;"
+# Check to see if Keyspaces are there. 
+docker exec music-db cqlsh -u cassandra -p cassandra -e "DESCRIBE keyspaces;"
+
+sleep 10;
+
+# Load data into Cassandra via Cassandra Job 
+echo "########## Running Cassandra Job (music-job) to load cql files ##########"
+docker run -d --name music-job --network music-net \
+-v $CQL_FILES/admin.cql:/cql/admin.cql \
+-v $CQL_FILES/admin_pw.cql:/cql/admin_pw.cql \
+-v $CQL_FILES/test.cql:/cql/extra/test.cql \
+-e PORT=9042 \
+-e CASS_HOSTNAME=music-db \
+-e USERNAME=$CASS_USERNAME \
+-e PASSWORD=$CASS_PASSWORD \
+$CASS_IMG_JOB
+# Logs
+echo "########## Cassandra Job logs ##########"
+docker logs music-job
+
 # Start Music war
+echo "########## Start music-war ##########"
 docker run -d --name music-war -v music-vol:/app ${MUSIC_IMG};
 
 # Start Zookeeper
+echo "########## Start zookeeper (music-zk) ##########"
 docker run -d --name music-zk --network music-net -p "2181:2181" -p "2888:2888" -p "3888:3888" ${ZK_IMG};
 
 ZOO_IP=`docker inspect -f '{{ $network := index .NetworkSettings.Networks "music-net" }}{{ $network.IPAddress}}' music-zk`
 echo "ZOOKEEPER_IP=${ZOO_IP}"
 
 # Delay  between Cassandra/Zookeeper and Tomcat
-sleep 60;
+sleep 10;
 
 # Start Up tomcat - Needs to have properties,logs dir and war file volume mapped.
+echo "########## Start Tomcat (music-tomcat) ##########"
 docker run -d --name music-tomcat --network music-net -p "8080:8080" -v music-vol:/usr/local/tomcat/webapps -v ${WORK_DIR}/properties:/opt/app/music/etc:ro -v ${WORK_DIR}/logs:/opt/app/music/logs ${TOMCAT_IMG};
 
 # Connect tomcat to host bridge network so that its port can be seen. 
+echo "########## Create Bridge for Tomcat ##########"
 docker network connect bridge music-tomcat;
 
 TOMCAT_IP=`docker inspect --format '{{ .NetworkSettings.Networks.bridge.IPAddress}}' music-tomcat`
@@ -80,20 +113,19 @@ echo "TOMCAT_IP=${TOMCAT_IP}"
 ${WORKSPACE}/test/csit/scripts/music/music-scripts/wait_for_port.sh ${TOMCAT_IP} 8080
 
 sleep 20;
-echo "get the tomcat logs to make sure its running music properly"
-echo "======== TOMCAT Logs =============="
+echo "########## TOMCAT Logs ##########"
 docker logs music-tomcat
 # Needed only if we need to look at localhost logs.
-#echo "===== MUSIC localhost Log ===================="
+#echo "########## MUSIC localhost Log ##########"
 #docker exec music-tomcat /bin/bash -c "cat /usr/local/tomcat/logs/localhost*"
 
-echo "===== MUSIC Log ===================="
+echo "########## MUSIC Log ##########"
 ls -al $MUSIC_LOGS/MUSIC
 docker exec music-tomcat /bin/bash -c "cat /opt/app/music/logs/MUSIC/music.log"
-echo "===== MUSIC error log =================="
+echo "########## MUSIC error log ##########"
 docker exec music-tomcat /bin/bash -c "cat /opt/app/music/logs/MUSIC/error.log"
 
-echo "inspect docker things for tracing purpose"
+echo "########## inspect docker things for tracing purpose ##########"
 docker inspect music-db
 docker inspect music-zk
 docker inspect music-tomcat
@@ -101,7 +133,7 @@ docker inspect music-war
 docker volume inspect music-vol
 docker network inspect music-net
 
-echo "dump music content just after music is started"
+echo "########## dump music content just after music is started ##########"
 docker exec music-db /usr/bin/nodetool status
 docker exec music-db /usr/bin/cqlsh -unelson24 -pwinman123 -e 'SELECT * FROM system_schema.keyspaces'
 docker exec music-db /usr/bin/cqlsh -unelson24 -pwinman123 -e 'DESCRIBE keyspace admin'
@@ -111,7 +143,7 @@ docker exec music-db /usr/bin/cqlsh -unelson24 -pwinman123 -e 'SELECT * FROM adm
 #
 # add here all ROBOT_VARIABLES settings
 #
-echo "# music robot variables settings";
+echo "########## music robot variables settings ##########";
 ROBOT_VARIABLES="-v MUSIC_HOSTNAME:http://${TOMCAT_IP} -v MUSIC_PORT:8080 -v COND_HOSTNAME:http://localhost -v COND_PORT:8091"
 
 echo ${ROBOT_VARIABLES}
