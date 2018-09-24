aa5b805bd7483d143046c516422a8b41c5f7a162
rajendrajaiswal <rajendra.jaiswal@ericsson.com>
2018-09-24 17:18:19 +0100

    Updated Setup and added testcase
    Issue-ID: INT-649
    Change-Id: I46e3acc7397e2f5945a043c98cfb03d4204ed4df
    Signed-off-by: Rajendra Jaiswal <rajendra.jaiswal@ericsson.com>

--- test/csit/plans/dcae-bulkpm/bulkpm-suite/teardown.sh
+++ test/csit/plans/dcae-bulkpm/bulkpm-suite/teardown.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo "Starting teardown script"
-cd $WORKSPACE/test/csit/scripts
-./kill-instance.sh $DMAAP
-./kill-instance.sh $KAFKA
-./kill-instance.sh $ZOOKEEPER
-./kill-instance.sh vescollector
-./kill-instance.sh datarouter-node
-./kill-instance.sh datarouter-prov
-./kill-instance.sh subscriber-node
-./kill-instance.sh mariadb
-./kill-instance.sh dfc
-./kill-instance.sh sftp
-sudo sed -i '/dmaap/d' /etc/hosts
+kill-instance.sh $DMAAP
+kill-instance.sh $KAFKA
+kill-instance.sh $ZOOKEEPER
+kill-instance.sh vescollector
+kill-instance.sh datarouter-node
+kill-instance.sh datarouter-prov
+kill-instance.sh subscriber-node
+kill-instance.sh mariadb
+kill-instance.sh dfc
+kill-instance.sh sftp
