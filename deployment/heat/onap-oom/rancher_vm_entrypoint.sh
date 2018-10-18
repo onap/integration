1ac488373e7edc0d12ceeedd184c252ba9b0a5a9
Gary Wu <gary.i.wu@huawei.com>
2018-10-18 16:08:19 -0700

    Re-enable use of RAM disk for /dockerdata-nfs
    Change-Id: Ib9bbcb3c5ceb83427b735ed061a921a28812a802
    Issue-ID: INT-586
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- deployment/heat/onap-oom/rancher_vm_entrypoint.sh
+++ deployment/heat/onap-oom/rancher_vm_entrypoint.sh
@@ -67,9 +67,9 @@ while ! hash jq &> /dev/null; do
 done
 
 # use RAM disk for /dockerdata-nfs for testing
-#echo "tmpfs /dockerdata-nfs tmpfs noatime 1 2" >> /etc/fstab
+echo "tmpfs /dockerdata-nfs tmpfs noatime,size=75% 1 2" >> /etc/fstab
 mkdir -pv /dockerdata-nfs
-#mount /dockerdata-nfs
+mount /dockerdata-nfs
 
 # version control the persistence volume to see what's happening
 chmod 777 /dockerdata-nfs/
@@ -84,7 +84,7 @@ git commit -m "initial commit"
 # export NFS mount
 NFS_EXP=""
 for K8S_VM_IP in $(tr -d ',[]' < /opt/config/k8s_private_ips.txt); do
-    NFS_EXP+="$K8S_VM_IP(rw,fsid=1,sync,no_root_squash,no_subtree_check) "
+    NFS_EXP+="$K8S_VM_IP(rw,fsid=1,async,no_root_squash,no_subtree_check) "
 done
 echo "/dockerdata-nfs $NFS_EXP" | tee /etc/exports
 
