e7f4e5e64679f1c4c028dcb5c150d3c2f30a4f7c
Rahul Tyagi <rahul.tyagi@ericsson.com>
2019-02-22 19:59:54 +0000

    INT-885:Enhancing the PNF-sim for netconf over TLS
    This commit add a script and a known_hosts file so that
    while starting the netopeer2 it readily start listening at
    6513 port for tls.
    
    This commit also adds a yang data model for testing of
    usecases developed under Netconf over tls usecase.
    
    Signed-off-by: Rahul Tyagi <rahul.tyagi@ericsson.com>
    Issue-ID: INT-885
    Change-Id: I26b059c293f0684f475f51618d905e10a9efe391
    Signed-off-by: Rahul Tyagi <rahul.tyagi@ericsson.com>

---
+++ test/mocks/pnfsimulator/netopeer_tls_cfg/update_tls.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+NETOPEER_CONFIG_PATH='/opt/dev/Netopeer2/server/configuration'
+MOUNT_PATH='/netopeer_tls_cfg'
+KEY_PATH='/usr/local/etc/keystored/keys'
+SUBSCRIBE_APP_PATH='/opt/dev/sysrepo/build/examples/application_changes_example'
+
+# This function uploads test_data and model into netopeer2 server
+upload_yang_data_model()
+{
+  sysrepoctl -i -g $MOUNT_PATH/building.yang
+  $SUBSCRIBE_APP_PATH building > /dev/null &
+  sysrepocfg --datastore=running --format=json building --import=$MOUNT_PATH/building.data
+}
+
+# This function configures server/trusted certificates into Netopeer
+configure_tls()
+{
+  sed -i "s/>test</>netconf</g" $NETOPEER_CONFIG_PATH/tls_listen.xml
+  sysrepocfg --datastore=running --format=xml ietf-keystore --merge=$NETOPEER_CONFIG_PATH/load_server_certs.xml
+  sysrepocfg --datastore=running --format=xml ietf-netconf-server --merge=$NETOPEER_CONFIG_PATH/tls_listen.xml
+}
+
+cp $MOUNT_PATH/test_server_key.pem $KEY_PATH
+cp $MOUNT_PATH/test_server_key.pem.pub $KEY_PATH
+configure_tls
+upload_yang_data_model
+
