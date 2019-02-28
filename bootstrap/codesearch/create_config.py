00a521616ee98778a6d7217f95577ca14689b40f
Pawel Wieczorek <p.wieczorek2@samsung.com>
2019-02-28 22:26:23 +0100

    Replace URLs to Gerrit repositories with their cgit mirrors
    Hound does not provide support for linking to source code in Gerrit
    repositories. Support for cgit mirrors can be achieved by using URL
    patterns.
    
    Issue-ID: ONAPARC-540
    Change-Id: Ib73b768d8a636680ab7e4d8d42463a16a9f45275
    Signed-off-by: Pawel Wieczorek <p.wieczorek2@samsung.com>

--- bootstrap/codesearch/create_config.py
+++ bootstrap/codesearch/create_config.py
@@ -8,6 +8,7 @@ import urllib.request
 import sys
 
 ONAP_GERRIT = "https://gerrit.onap.org/r"
+ONAP_CGIT = "https://git.onap.org"
 API_PROJECTS = "/projects/"
 
 MAGIC_PREFIX = ")]}'"
@@ -27,7 +28,7 @@ def get_projects_list():
 
 def create_repos_list(projects):
     """Create a map of all projects to their repositories' URLs."""
-    return {p: {"url": "{}/{}.git".format(ONAP_GERRIT, p)} for p in projects}
+    return {p: {"url": "{}/{}".format(ONAP_CGIT, p), "url-pattern": {"base-url": "{url}/tree/{path}{anchor}", "anchor": "#n{line}"}} for p in projects}
 
 
 def main():
