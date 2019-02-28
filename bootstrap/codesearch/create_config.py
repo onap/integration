d18981c93098601f1d50d220121970cb6e3a3561
Pawel Wieczorek <p.wieczorek2@samsung.com>
2019-02-28 22:00:18 +0100

    Add Hound code search configuration generator
    Configuration is generated from Gerrit-supplied data.
    
    Issue-ID: ONAPARC-537
    Change-Id: I84d5b87580882926b916ed20dbcd2369be4c77f4
    Signed-off-by: Pawel Wieczorek <p.wieczorek2@samsung.com>

---
+++ bootstrap/codesearch/create_config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Create configuration for code search."""
+
+import json
+import urllib.request
+import sys
+
+ONAP_GERRIT="https://gerrit.onap.org/r"
+API_PROJECTS="/projects/"
+
+MAGIC_PREFIX=")]}'"
+
+def get_projects_list():
+    """Request list of all available projects from ONAP Gerrit."""
+    resp = urllib.request.urlopen(ONAP_GERRIT + API_PROJECTS)
+    resp_body = resp.read()
+
+    no_magic = resp_body[len(MAGIC_PREFIX):]
+    decoded = no_magic.decode("utf-8")
+    projects = json.loads(decoded)
+
+    return projects.keys()
+
+
+def create_repos_list(projects):
+    """Create a map of all projects to their repositories' URLs."""
+    return {p: {"url": "{}/{}.git".format(ONAP_GERRIT, p)} for p in projects}
+
+
+def main():
+    """Main entry point for the script."""
+    repos = create_repos_list(get_projects_list())
+    config = {
+        "max-concurrent-indexers": 2,
+        "dbpath": "data",
+        "health-check-uri": "/healthz",
+        "repos": repos
+    }
+    print(json.dumps(config, sort_keys=True, indent=4, separators=(',', ': ')))
+
+
+if '__main__' == __name__:
+    sys.exit(main())
