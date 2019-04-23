6ab8b62c35c6c850b95dbb3206de78a6ddb1206d
Enbo Wang <wangenbo@huawei.com>
2019-04-23 13:42:21 +0000

    Add an EMS simulator
    Change-Id: I4fedf9a812e19033e7f9a1bff55eae264bc5122f
    Issue-ID: INT-1041
    Signed-off-by: Enbo Wang <wangenbo@huawei.com>

---
+++ test/mocks/emssimulator/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+readonly old_pwd=$PWD
+cd swm/sw_server_simulator
+
+for i in `ls -1`; do
+	if [ -d $i ]; then
+		cd $i
+		echo $i
+		zip -r ${i}.zip *
+		mv ${i}.zip ..
+		cd $OLDPWD
+	fi
+done
+
+cd $old_pwd
+
