ecba22c3fd5aab50eb637c970d08e94269f6fde9
Yang Xu <yxu@futurewei.com>
2019-06-07 14:52:40 -0400

    Add sleep time before commands when redeploying
    Signed-off-by: Yang Xu <yang.xu@futurewei.com>
    Issue-ID: INT-1098
    Change-Id: If5dfff8fe63be09c7016d593b0e8c403e80fb782

--- deployment/heat/onap-rke/scripts/redeploy-module.sh
+++ deployment/heat/onap-rke/scripts/redeploy-module.sh
@@ -17,9 +17,22 @@ fi
 module=$1
 deploy=dev-$1
 cd /root/oom/kubernetes
+echo "Deleting $deploy ..."
 helm delete $deploy --purge
+echo "Wait for 5 seconds before cleaning up deployment resource ..."
+sleep 5
+echo "Cleaning up deployment resource ..."
 /root/integration/deployment/heat/onap-rke/scripts/cleanup.sh $module
+echo "Wait for 5 seconds before cleaning up deployment file system ..."
+sleep 5
+echo "Cleaning up deployment file system ..."
 rm -rf /dockerdata-nfs/$deploy
+echo "Wait for 5 seconds before make $module and make onap ..."
+sleep 5
+echo "making $module and making onap ..."
 make $module
 make onap 
+echo "Wait for 5 seconds before deploying $deploy ..."
+sleep 5
+echo "Deploying $deploy ..."
 helm deploy $deploy local/onap -f /root/oom/kubernetes/onap/resources/environments/public-cloud.yaml -f /root/integration-override.yaml --namespace onap
