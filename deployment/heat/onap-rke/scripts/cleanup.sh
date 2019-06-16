2563a5e70e720d2aef74fce3c7b4be8afd38e8fc
Yang Xu <yang.xu@futurewei.com>
2019-06-15 23:59:51 -0400

    Remove shared keyspaces for SDC in cleanup
    Signed-off-by: Yang Xu <yang.xu@futurewei.com>
    Issue-ID: INT-1103
    Change-Id: I5194151ea6baadb455eb3e4c8d157012c6cfc371

--- deployment/heat/onap-rke/scripts/cleanup.sh
+++ deployment/heat/onap-rke/scripts/cleanup.sh
@@ -15,6 +15,12 @@ if [ $COMPONENT == "dcae" ] || [ $COMPONENT == "DCAE" ]; then
 	kubectl delete service consul -n onap
 fi
 
+if [ $COMPONENT == "sdc" ] || [ $COMPONENT == "SDC" ]; then
+        for keyspace in sdctitan sdcrepository sdcartifact sdccomponent sdcaudit; do
+	        kubectl exec -it dev-cassandra-cassandra-0 -- cqlsh -u cassandra -p cassandra -e "drop keyspace ${keyspace}"
+        done
+fi
+
 for op in secrets configmaps pvc pv services deployments statefulsets clusterrolebinding; do
 	ARRAY=(`kubectl get $op -n onap | grep dev-$COMPONENT | awk '{print $1}'`)
 	for i in ${ARRAY[*]}; do
