dc39c641935041ecd96214f552f1e4c05ddc24c8
Pawel Wieczorek <p.wieczorek2@samsung.com>
2019-06-27 18:11:16 +0200

    k8s: Make Rancher CLI provisioner configurable
    Apparently Linux AMD64 machines are not the only ones used for ONAP
    development (author meant no harm).
    
    Issue-ID: SECCOM-235
    Signed-off-by: Pawel Wieczorek <p.wieczorek2@samsung.com>
    Change-Id: Ia78a02fb82dc5752d6b8fd2cef8e6ef583fd3ca6

--- test/security/k8s/tools/get_ranchercli.sh
+++ test/security/k8s/tools/get_ranchercli.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/env bash
 
+# Constants
+DEFAULT_VERSION='v0.6.12'
+DEFAULT_ARCH='amd64'
+DEFAULT_SYSTEM='linux'
+
+# Variables
+VERSION="${1:-$DEFAULT_VERSION}"
+ARCH="${2:-$DEFAULT_ARCH}"
+SYSTEM="${3:-$DEFAULT_SYSTEM}"
+
+ARCHIVE="rancher-${SYSTEM}-${ARCHITECTURE}-${VERSION}.tar.gz"
+DIRECTORY="rancher-${VERSION}"
+URL="https://releases.rancher.com/cli/${VERSION}/${ARCHIVE}"
+
+
 # Prerequistes
-wget https://releases.rancher.com/cli/v0.6.12/rancher-linux-amd64-v0.6.12.tar.gz
-tar xf rancher-linux-amd64-v0.6.12.tar.gz
+wget "$URL"
+tar xf "$ARCHIVE"
 
 # Installation
 echo '# Privilege elevation needed to move Rancher CLI binary to /usr/local/bin'
-sudo mv rancher-v0.6.12/rancher /usr/local/bin/
+sudo mv "${DIRECTORY}/rancher" /usr/local/bin/
 
 # Cleanup
-rmdir rancher-v0.6.12/
-rm rancher-linux-amd64-v0.6.12.tar.gz
+rmdir "$DIRECTORY"
+rm "$ARCHIVE"
