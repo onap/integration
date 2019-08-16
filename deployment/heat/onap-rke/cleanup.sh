69a99444797e55697f29e3ec51c7e35ef9d129f3
Marco Platania <platania@research.att.com>
2019-08-16 10:32:58 -0400

    Make cleanup script more generic
    Issue-ID: INT-1073
    
    Signed-off-by: Marco Platania <platania@research.att.com>
    Change-Id: Ic01eb302d92d893d17eb71a9341be514cb12dc3f

---
+++ deployment/heat/onap-rke/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IFS='
+'
+
+if [ -z $1 ]; then
+	echo "ONAP component name missing"
+	echo "Usage: ./cleanup.sh onap_component_name"
+	exit 1
+fi
+
+( cd scripts; ./cleanup.sh onap dev $1 )
