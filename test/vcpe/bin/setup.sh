d0ad78f758c9f81b02708460bc840f8bed36dd96
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2019-09-30 15:01:16 +0200

    Refactor setup script for preparing vCPE tools runtime env
    Script is now more generic and allows proper setup
    of vcpe scripts runtime environment on Ubuntu 16.04/18.04
    and on Centos/Rhel 7.6.
    
    Change-Id: I7b7d944f5a6a7a9dc45921082f908a1f8aa185a1
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>
    Issue-ID: TEST-203

--- test/vcpe/bin/setup.sh
+++ test/vcpe/bin/setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-# 
-# Copyright 2019 Futurewei Technologies Co., Ltd.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
 
-apt-get install -y python-pip
-pip install ipaddress
-pip install pyyaml
-pip install mysql-connector-python
-pip install progressbar2
-pip install python-novaclient
-pip install python-openstackclient
-pip install python-heatclient
-pip install netaddr
-pip install kubernetes
+# This script prepares the runtime environment
+# for running vCPE python scripts on Ubuntu 16.04,
+# 18.04 and on Centos/Rhel 7.6.
+
+if command -v apt-get > /dev/null; 
+then
+    apt-get update
+    apt-get -y install python gcc python-dev;
+fi
+if command -v yum > /dev/null;
+then
+    yum -y install python-devel gcc;
+fi
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+pip install -I \
+    ipaddress \
+    pyyaml \
+    mysql-connector-python \
+    progressbar2 \
+    python-novaclient \
+    python-openstackclient \
+    python-heatclient \
+    kubernetes \
+    netaddr
