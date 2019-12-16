2ef4e76245215e76723f5c19ae5eb31687a7ba24
rajendrajaiswal <rajendra.jaiswal@ericsson.com>
2019-12-16 14:24:02 +0000

    PNF Simulator to support Control Loop subscription model
    Change-Id: I9919edb32f3f68f86fad28c908f808fcee3fc548
    Issue-ID: INT-1312
    Signed-off-by: rajendrajaiswal <rajendra.jaiswal@ericsson.com>

---
+++ test/mocks/pmsh-pnf-sim/docker-compose/pnfconfig.py
@@ -0,0 +1,7 @@
+import os
+
+VES_IP = "10.209.57.227"
+VES_PORT = "30235"
+script_dir = os.path.dirname(__file__)
+rel_path = "FileReadyEvent.json"
+fileReadyEventPath = os.path.join(script_dir,rel_path)
