9665859f4902451237d73eb9c2c74283e3569c3b
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2019-12-17 15:09:26 +0100

    Support setting custom path to config file in helper scripts
    Change-Id: I43b160cfc3d087a0e8b70c9fb4cafa3626f84db8
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>
    Issue-ID: INT-1399

--- test/vcpe/get_info.py
+++ test/vcpe/get_info.py
@@ -11,11 +11,18 @@ import base64
 from vcpecommon import *
 import preload
 import vcpe_custom_service
+import argparse
 
+# Run the script with [-h|--help] to get usage info
 
 logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-vcpecommon = VcpeCommon()
+parser = argparse.ArgumentParser(formatter_class=
+                                 argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--config',help='Configuration file path',default=None)
+args = parser.parse_args()
+
+vcpecommon = VcpeCommon(cfg_file=args.config)
 nodes=['brg', 'bng', 'mux', 'dhcp']
 hosts = vcpecommon.get_vm_ip(nodes)
 print(json.dumps(hosts, indent=4, sort_keys=True))
