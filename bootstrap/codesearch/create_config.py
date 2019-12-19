8a0ce7637ba45132e98c99d2b7a8603e1e2db64c
Pawel Wieczorek <p.wieczorek2@samsung.com>
2019-12-19 13:23:47 +0100

    Add support for using SSH
    This patch changes "create_config.py" script interface and its
    behaviour:
    
    * by default anonymous Gerrit is used for repository cloning and code
      URLS,
    * external git mirror for code URLs might be provided but there is no
      default one,
    * SSH credentials might be provided for repository cloning.
    
    Cloning repository using SSH is required due to current ONAP
    infrastructure HTTPS bandwidth constraints.
    
    User wishing to use this setup should provide two environmental
    variables:
    
    1) API_USER - Gerrit username for cloning repositories via SSH
    2) API_KEY - Gerrit API key (User menu -> Settings -> HTTP Credentials)
    
    Be aware that a new RSA key will be generated and added to the user's
    Gerrit account for purposes of Hound code search operation inside a VM.
    Do remember to delete it (User menu -> Settings -> SSH keys) after
    evaluating this environment.
    
    Issue-ID: ONAPARC-537
    Change-Id: I3e98dfa8582322d5b3bbbbf377d748faea0da57b
    Signed-off-by: Pawel Wieczorek <p.wieczorek2@samsung.com>

--- bootstrap/codesearch/create_config.py
+++ bootstrap/codesearch/create_config.py
@@ -8,15 +8,15 @@ import json
 import urllib.request
 import sys
 
-DEFAULT_GERRIT="https://gerrit.onap.org/r"
-DEFAULT_GIT="https://git.onap.org"
+DEFAULT_GERRIT="gerrit.onap.org"
+API_PREFIX="/r"
 API_PROJECTS="/projects/"
 
 MAGIC_PREFIX=")]}'"
 
 def get_projects_list(gerrit):
     """Request list of all available projects from ONAP Gerrit."""
-    resp = urllib.request.urlopen(gerrit + API_PROJECTS)
+    resp = urllib.request.urlopen("https://{}{}{}".format(gerrit, API_PREFIX, API_PROJECTS))
     resp_body = resp.read()
 
     no_magic = resp_body[len(MAGIC_PREFIX):]
@@ -26,17 +26,19 @@ def get_projects_list(gerrit):
     return projects.keys()
 
 
-def create_repos_list(projects, gitweb, git, gerrit):
+def create_repos_list(projects, gerrit, ssh, git):
     """Create a map of all projects to their repositories' URLs."""
     repos_list = {}
     for p in projects:
-        if gitweb:
-            project_url = "{}/{}.git".format(gerrit, p)
-            code_url = "{}/gitweb?p={}.git;hb=HEAD;a=blob;f={{path}}{{anchor}}".format(gerrit, p)
-            anchor = "#l{line}"
-        else:
-            project_url = "{}/{}".format(git, p)
-            code_url = "{url}/tree/{path}{anchor}"
+        project_url = "https://{}{}/{}.git".format(gerrit, API_PREFIX, p)
+        code_url = "https://{}{}/gitweb?p={}.git;hb=HEAD;a=blob;f={{path}}{{anchor}}".format(gerrit, API_PREFIX, p)
+        anchor = "#l{line}"
+
+        if ssh and len(ssh) == 2:
+            user, port = ssh[0], ssh[1]
+            project_url = "ssh://{}@{}:{}/{}.git".format(user, gerrit, port, p)
+        if git:
+            code_url = "https://{}/{}/tree/{{path}}{{anchor}}".format(git, p)
             anchor = "#n{line}"
 
         repos_list[p] = {
@@ -54,9 +56,8 @@ def parse_arguments():
     """Return parsed command-line arguments."""
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--gerrit', help='Gerrit address', default=DEFAULT_GERRIT)
-    access = parser.add_mutually_exclusive_group()
-    access.add_argument('--gitweb', help='use Gerrit\'s gitweb (bool)', action='store_true')
-    access.add_argument('--git', help='use external git (address)', default=DEFAULT_GIT)
+    parser.add_argument('--ssh', help='SSH information: user, port', nargs=2)
+    parser.add_argument('--git', help='external git address')
 
     return parser.parse_args()
 
@@ -66,7 +67,7 @@ def main():
     arguments = parse_arguments()
 
     projects = get_projects_list(arguments.gerrit)
-    repos = create_repos_list(projects, arguments.gitweb, arguments.git, arguments.gerrit)
+    repos = create_repos_list(projects, arguments.gerrit, arguments.ssh, arguments.git)
     config = {
         "max-concurrent-indexers": 2,
         "dbpath": "data",
