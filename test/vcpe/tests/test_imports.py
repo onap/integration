4ed26357542880e40b404e40c4e4c60cfd9e9060
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-02-04 15:09:29 +0100

    Add test to ensure all needed modules are provided by setuptools
    Change-Id: I592a6bf393007612c263f6e54aabfcdb6738141a
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>
    Issue-ID: INT-1427

---
+++ test/vcpe/tests/test_imports.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('./')
+
+import vcpecommon
+import config_sdnc_so
+import csar_parser
+import preload
+import sdcutils
+import soutils
+import vcpe_custom_service
+import vcpecommon
+
+# This will test whether all modules that vcpe scripts leverage
+# are included in setuptools configuration
+
+def test_imports():
+  pass
