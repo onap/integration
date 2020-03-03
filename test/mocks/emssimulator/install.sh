818f76a0173c5d6cef483425042b4f0651ee793b
Enbo Wang <wangenbo@huawei.com>
2020-03-04 00:42:31 +0800

    EMS simulator extension for PNF SW Upgrade
    Change-Id: I3a8c706373f4004850c2403f4aee0d1f28aad464
    Issue-ID: INT-1208
    Signed-off-by: Enbo Wang <wangenbo@huawei.com>

--- test/mocks/emssimulator/install.sh
+++ test/mocks/emssimulator/install.sh
@@ -4,13 +4,13 @@ readonly old_pwd=$PWD
 cd swm/sw_server_simulator
 
 for i in `ls -1`; do
-	if [ -d $i ]; then
-		cd $i
-		echo $i
-		zip -r ${i}.zip *
-		mv ${i}.zip ..
-		cd $OLDPWD
-	fi
+    if [ -d $i ]; then
+        cd $i
+        echo $i
+        zip -r ${i}.zip *
+        mv ${i}.zip ..
+        cd $OLDPWD
+    fi
 done
 
 cd $old_pwd
