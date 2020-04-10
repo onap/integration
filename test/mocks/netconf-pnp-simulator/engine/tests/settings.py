1870f59444292d0409216bea9b79061493638ea1
ebo <eliezio.oliveira@est.tech>
2020-04-10 21:17:17 +0100

    netconf-pnp-simulator: normalized image reference
    Required change for the forthcoming fix on tox execution in
    ci-management/jjb/integration/integration-docker.yaml
    
    IMPORTANT: Merge https://gerrit.onap.org/r/c/integration/+/105797 first
    
    Issue-ID: INT-1124
    Change-Id: I70c3351e5cf691a9eaeb7b49ec276d825016e0fa
    Signed-off-by: ebo <eliezio.oliveira@est.tech>

--- test/mocks/netconf-pnp-simulator/engine/tests/settings.py
+++ test/mocks/netconf-pnp-simulator/engine/tests/settings.py
@@ -2,9 +2,7 @@ import os
 
 HOST = "127.0.0.1"
 # Set by tox-docker
-# Unexpectedly, tox-docker uses the repository prefix instead of the image name to define the
-# variable prefix.
-PORT = int(os.environ["LOCALHOST_830_TCP_PORT"])
+PORT = int(os.environ["NETCONF_PNP_SIMULATOR_830_TCP_PORT"])
 USERNAME = "netconf"
 KEY_FILENAME = "../config/ssh/id_rsa"
 
