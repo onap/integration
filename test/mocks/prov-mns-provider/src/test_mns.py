51b8ecb9be73dfda3fd96ec5ce4bb134601109d7
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-04-14 08:49:19 -0700

    Extend unit tests for provisioning management service
    This adds additional unit tests for the service provider for
    PATCH and DELETE request methods.
    
    Change-Id: If0cce9c713f2999c859910661308c24ac68f04d6
    Issue-ID: INT-1529
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- test/mocks/prov-mns-provider/src/test_mns.py
+++ test/mocks/prov-mns-provider/src/test_mns.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 MOI_ID = str(uuid4())
 MOI_CLASS = ProvMnSProvider.Cretaed_MOIs_list[0]['class']
 MOI_DATA_TMPL = { 'data': ProvMnSProvider.Cretaed_MOIs_list[0] }
+MOI_DATA_PATCH = { "data": { "pLMNId": "xxx", "gNBId": "1234", "gNBIdLength": "4" }}
 URI_SCHEMA = 'http'
 AUTH_STRING = (ProvMnSProvider.username, ProvMnSProvider.password)
 URI_BASE_STRING = URI_SCHEMA + '://' + ProvMnSProvider.ipAddress + ':' + \
@@ -47,3 +48,27 @@ def test_get():
         logger.debug('Response content: {0}'.format(req_get.text))
 
     assert req_get.status_code == requests.codes.ok
+
+def test_patch():
+    '''Validate PATCH request'''
+
+    req_patch = requests.patch('{0}'.format(URI_PATCH_STRING),
+                               auth=AUTH_STRING, json=MOI_DATA_PATCH)
+
+    if req_patch.status_code != requests.codes.ok:
+        logger.error('PATCH request to {0} failed'.format(URI_PATCH_STRING))
+        logger.debug('Response content: {0}'.format(req_patch.text))
+
+    assert req_patch.status_code == requests.codes.ok
+
+def test_delete():
+    '''Validate DELETE request'''
+
+    req_delete = requests.delete('{0}'.format(URI_DELETE_STRING),
+                                 auth=AUTH_STRING)
+
+    if req_delete.status_code != requests.codes.ok:
+        logger.error('DELETE request to {0} failed'.format(URI_DELETE_STRING))
+        logger.debug('Response content: {0}'.format(req_delete.text))
+
+    assert req_delete.status_code == requests.codes.ok
