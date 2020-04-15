05021914b2e3d554fa5d29717975f9d87a01c6a7
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-04-15 04:48:16 -0700

    Move tests to their own directory
    File layout was also restructured for better
    readability.
    
    Change-Id: Id1cba755127319c45ec50b08d12daa543c8c9ae5
    Issue-ID: INT-1529
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- test/mocks/prov-mns-provider/src/tests/test_rest_api_endpoints.py
+++ test/mocks/prov-mns-provider/src/tests/test_rest_api_endpoints.py
@@ -1,26 +1,4 @@
-import requests
-from uuid import uuid4
-import ProvMnSProvider
-import logging
-from json import dumps
-
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
-MOI_ID = str(uuid4())
-MOI_CLASS = ProvMnSProvider.Cretaed_MOIs_list[0]['class']
-MOI_DATA_TMPL = { 'data': ProvMnSProvider.Cretaed_MOIs_list[0] }
-MOI_DATA_PATCH = { "data": { "pLMNId": "xxx", "gNBId": "1234", "gNBIdLength": "4" }}
-URI_SCHEMA = 'http'
-AUTH_STRING = (ProvMnSProvider.username, ProvMnSProvider.password)
-URI_BASE_STRING = URI_SCHEMA + '://' + ProvMnSProvider.ipAddress + ':' + \
-             str(ProvMnSProvider.portNumber) + ProvMnSProvider.prefix + \
-             '/' + MOI_CLASS + '/' + MOI_ID
-URI_PUT_STRING = URI_BASE_STRING
-URI_GET_STRING = URI_BASE_STRING + '?scope=BASE_ONLY&filter=' + MOI_CLASS + \
-                 '&fields=gNBId&fields=gNBIdLength'
-URI_PATCH_STRING = URI_BASE_STRING + '?scope=BASE_ONLY&filter=' + MOI_CLASS
-URI_DELETE_STRING = URI_PATCH_STRING
+from common import *
 
 def test_put():
     '''Validate PUT request'''
