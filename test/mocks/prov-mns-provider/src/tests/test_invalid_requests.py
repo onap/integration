fffee6dbfc99d1b1fd750efc4bb9b4185c66faa4
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-04-16 04:11:07 -0700

    Add test to verify bad API prefix handling by the service
    Functional test for verifying that the service handles
    bad API prefix in URI string properly and returns appropriate
    response to the client.
    
    Change-Id: I5e5e8a9dcd6fe05bd2b4536790d16e825aa21679
    Issue-ID: INT-1529
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- test/mocks/prov-mns-provider/src/tests/test_invalid_requests.py
+++ test/mocks/prov-mns-provider/src/tests/test_invalid_requests.py
@@ -47,3 +47,29 @@ def test_bad_moi_class():
                           auth=AUTH_STRING)
     assert req.status_code == requests.codes.not_acceptable
     assert INVALID_CLASS_MSG in req.text
+
+def test_bad_prefix():
+    '''Check service returns proper
+    http code and error msg if URI prefix
+    is invalid'''
+
+    for url in BAD_PREFIX_URI_BASE_STRING, BAD_PREFIX1_URI_BASE_STRING:
+        req = requests.get('{0}'.format(url),
+                           auth=AUTH_STRING)
+        assert req.status_code == requests.codes.not_found
+        assert INVALID_PREFIX_MSG in req.text
+
+        req = requests.put('{0}'.format(url),
+                           auth=AUTH_STRING, json=MOI_DATA_TMPL)
+        assert req.status_code == requests.codes.not_found
+        assert INVALID_PREFIX_MSG in req.text
+
+        req = requests.patch('{0}'.format(url),
+                             auth=AUTH_STRING, json=MOI_DATA_PATCH)
+        assert req.status_code == requests.codes.not_found
+        assert INVALID_PREFIX_MSG in req.text
+
+        req = requests.delete('{0}'.format(url),
+                              auth=AUTH_STRING)
+        assert req.status_code == requests.codes.not_found
+        assert INVALID_PREFIX_MSG in req.text
