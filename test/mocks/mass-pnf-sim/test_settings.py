a5bb75c29de07b85ab9829fb0dd8cf173863feb7
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-05-25 00:49:14 -0700

    Add pytest testing scenario settings for simulator
    Change-Id: I57d67a837aa3ef8d6a58e5abbb61bfc3606e93dc
    Issue-ID: INT-1577
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

---
+++ test/mocks/mass-pnf-sim/test_settings.py
@@ -0,0 +1,6 @@
+SIM_INSTANCES = 2
+URLVES = 'http://127.0.0.1:10000/eventListener/v7'
+IPFILESERVER = '127.0.0.1'
+TYPEFILESERVER = 'sftp'
+IPSTART = '10.11.0.1'
+INSTANCE_CONFIG = 'config/config.yml'
