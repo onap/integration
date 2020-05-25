590378136f9ff1333465825c162522b3da5352b4
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-05-25 00:54:12 -0700

    Add pytest fixtures
    This adds fixtures that'll be utilised by unit
    and functional tests.
    
    Change-Id: Iaeed869d8320ccc44d485fc00c590741352e34e6
    Issue-ID: INT-1577
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

---
+++ test/mocks/mass-pnf-sim/conftest.py
@@ -0,0 +1,30 @@
+import MassPnfSim
+import pytest
+from test_settings import * # pylint: disable=W0614
+
+@pytest.fixture(scope="module")
+def parser():
+    return MassPnfSim.get_parser()
+
+@pytest.fixture(scope="module")
+def args_bootstrap(parser):
+    return parser.parse_args(['bootstrap', '--count', str(SIM_INSTANCES),
+                             '--urlves', URLVES, '--ipfileserver', IPFILESERVER,
+                             '--typefileserver', TYPEFILESERVER, '--ipstart',
+                             IPSTART])
+
+@pytest.fixture(scope="module")
+def args_start(parser):
+    return parser.parse_args(['start'])
+
+@pytest.fixture(scope="module")
+def args_stop(parser):
+    return parser.parse_args(['stop'])
+
+@pytest.fixture(scope="module")
+def args_status(parser):
+    return parser.parse_args(['status'])
+
+@pytest.fixture(scope="module")
+def args_trigger(parser):
+    return parser.parse_args(['trigger'])
