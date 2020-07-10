057fdc47af05019b4dab01f1cc142a4763392dd5
efiacor <fiachra.corcoran@est.tech>
2020-07-10 14:58:50 +0100

    [PMSHSIM] Refactoring pmsh-pnf-sim
    Signed-off-by: efiacor <fiachra.corcoran@est.tech>
    Change-Id: I40834e906fcc5f33c6e70fb3d3da886a7ee0d521
    Issue-ID: INT-1657

--- test/mocks/pmsh-pnf-sim/docker-compose/schedulepmjob.py
+++ test/mocks/pmsh-pnf-sim/docker-compose/schedulepmjob.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
-
+import logging.config
+import os
+import sys
 import time
+
 import schedule
+import yaml
+
+from app_config import pnfconfig
 from pnf import PNF
-import pnfconfig
+
+log_file_path = os.path.join(os.path.dirname(__file__), 'app_config/logger_config.yaml')
+with open(log_file_path, 'r') as f:
+    log_cfg = yaml.safe_load(f.read())
+logging.config.dictConfig(log_cfg)
+logger = logging.getLogger('dev')
 
 if __name__ == "__main__":
     try:
-        pnf = PNF()
-        schedule.every(pnfconfig.ROP).seconds.do(pnf.pm_job)
+        schedule.every(pnfconfig.ROP).seconds.do(PNF.pm_job)
+        logger.info('Starting PM scheduling job')
         while True:
             schedule.run_pending()
             time.sleep(1)
     except Exception as error:
-        print(error)
+        logger.debug(error)
+        sys.exit(1)
