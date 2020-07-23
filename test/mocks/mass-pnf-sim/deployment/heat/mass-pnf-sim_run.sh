3ad7e505bc57626f95e54670d13903366b703f55
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-07-23 11:08:36 +0200

    Add HOT parameters for the simulator
    Run simulator instances from runner script
    
    Change-Id: I9d9a317c378a84b1846821916fee5f47452944c5
    Issue-ID: INT-1662
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

--- test/mocks/mass-pnf-sim/deployment/heat/mass-pnf-sim_run.sh
+++ test/mocks/mass-pnf-sim/deployment/heat/mass-pnf-sim_run.sh
@@ -4,3 +4,8 @@ git clone https://git.onap.org/integration
 pushd integration/test/mocks/mass-pnf-sim
 ./setup.sh
 source .tox/MassPnfSim-runtime/bin/activate
+./mass-pnf-sim.py build
+./mass-pnf-sim.py bootstrap --count $SIMULATOR_INSTANCES --urlves $VES_URL --ipfileserver $FILE_SERVER --typefileserver ftps --ipstart 10.11.0.16 --user $FTP_USER --password $FTP_PASSWORD
+./mass-pnf-sim.py start
+sleep 30
+./mass-pnf-sim.py trigger
