c0d257ad70ed4306a2fd54055e9d31e71e0d6732
Enbo Wang <wangenbo@huawei.com>
2020-09-18 11:06:12 +0800

    Add an External RAN NSSMF simulator
    Change-Id: I770091159dd1f953d8a567ccd2bd7f6d21f27649
    Issue-ID: INT-1707
    Signed-off-by: Enbo Wang <wangenbo@huawei.com>

---
+++ test/mocks/ran-nssmf-simulator/ran_nssmf/NssManager.py
@@ -0,0 +1,48 @@
+#  ============LICENSE_START=======================================================
+#  Copyright (C) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+#  ================================================================================
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ============LICENSE_END=========================================================
+
+import uuid
+
+from utils import getLogger
+
+
+logger = getLogger("NssManager")
+
+
+class NssError(ValueError):
+    pass
+
+
+def allocateNssi(requestBody):
+    sliceProfile = requestBody["attributeListIn"]
+    sliceProfileId = sliceProfile["sliceProfileId"]
+
+    nSSId = uuid.uuid4().hex
+
+    responseBody = {
+        "attributeListOut": {},
+        "href": nSSId
+    }
+
+    logger.info("Allocate NSSI for sliceProfileId %s success, nSSId: %s" % (sliceProfileId, nSSId))
+    return responseBody
+
+
+def deallocateNssi(sliceProfileId, requestBody):
+    nSSId = requestBody["nSSId"]
+
+    logger.info("Deallocate NSSI for sliceProfileId %s success, nSSId: %s" % (sliceProfileId, nSSId))
+    return ""
