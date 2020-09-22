58cb8d0e6d99a6860762b92b6d168b5b9d5607f6
Bartek Grzybowski <b.grzybowski@partner.samsung.com>
2020-09-22 12:54:17 +0200

    Setup basic functional test of NSSMF RAN simulator
    This setup will be leveraged in CI for functional
    verification of simulator.
    
    Change-Id: I2e3e501c9eabfc3aadbfbb256e1e22ae2bf0221d
    Issue-ID: INT-1723
    Signed-off-by: Bartek Grzybowski <b.grzybowski@partner.samsung.com>

---
+++ test/mocks/ran-nssmf-simulator/test/test_main.py
@@ -0,0 +1,10 @@
+from requests import post, codes
+from test_settings import TEST_REST_URL, TEST_REST_GET_ACCESS_TOKEN_ENDPOINT, TEST_REST_HEADERS
+
+def test_get_auth_token(auth_credentials):
+    url = f"{TEST_REST_URL}{TEST_REST_GET_ACCESS_TOKEN_ENDPOINT}"
+    response = post(url, headers=TEST_REST_HEADERS, verify=False, json=auth_credentials[0])
+    json_response = response.json()
+    assert "accessToken" in json_response
+    assert "expires" in json_response
+    assert response.status_code == codes.created
---
+++ test/mocks/ran-nssmf-simulator/test/test_settings.py
@@ -0,0 +1,6 @@
+TEST_AUTH_DB_FILE = "test/test_auth.json"
+TEST_REST_PORT = 8443
+TEST_REST_IP = "127.0.0.1"
+TEST_REST_URL = f"https://{TEST_REST_IP}:{TEST_REST_PORT}"
+TEST_REST_GET_ACCESS_TOKEN_ENDPOINT = "/api/rest/securityManagement/v1/oauth/token"
+TEST_REST_HEADERS = { "Content-Type": "application/json" }
